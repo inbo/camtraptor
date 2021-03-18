023ac6876bb1d884cbe915f28c26b7e8fb065e7c
damianooldoni <damiano.oldoni@inbo.be>
2021-03-18 21:06:12 +0100

    Hard code name of files in data package

--- R/read_camtrap-dp.R
+++ R/read_camtrap-dp.R
@@ -13,33 +13,35 @@
 #' @return a list of tibbles (dataframes)
 #'
 #' @examples
-#' \dontrun {
+#' \dontrun{
 #' library(here)
 #' camtrap_dp_folder <- here("inst", "extdata", "gmu8-monitoring-faunabeheerzone-8-20210301093537")
 #' gmu8 <- read_camtrap_dp(camtrap_dp_folder)
 #' }
-read_camtrap_dp <- function(camtrap_dp_folder,
-                            datapackage = "datapackage.json",
-                            deployments = "deployments.csv",
-                            multimedia = "multimedia.csv",
-                            observations = "observations.csv") {
+read_camtrap_dp <- function(path) {
   # add asserts to check presence of the 4 files
   # TO BE DONE
 
   # check uniqueness PKs (ids) to be sure no problems arise in other functions
   # TO BE DONE
 
+  # define files
+  datapackage <- "datapackage.json"
+  deployments <- "deployments.csv"
+  multimedia <- "multimedia.csv"
+  observations <- "observations.csv"
+
   # read files
-  datapackage <- read_json(path = here(camtrap_dp_folder, datapackage))
-  deployments <- read_csv(file = here(camtrap_dp_folder, deployments))
+  datapackage <- read_json(path = here(path, datapackage))
+  deployments <- read_csv(file = here(path, deployments))
   multimedia <- read_csv(
-    file = here(camtrap_dp_folder, multimedia),
+    file = here(path, multimedia),
     col_types = cols(
       comments = col_character()
     )
   )
   observations <- read_csv(
-    file = here(camtrap_dp_folder, observations),
+    file = here(path, observations),
     col_types = cols(
       classification_confidence = col_number()
     )

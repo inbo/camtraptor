7a43b41685f7f59ee3398dba73db8d5dae2fcc01
damianooldoni <damiano.oldoni@inbo.be>
2021-03-18 20:50:35 +0100

    Add hover_column as arg to function

--- R/visualize_species_per_deployment.R
+++ R/visualize_species_per_deployment.R
@@ -7,27 +7,47 @@
 #' @param observations a tibble (data.frame) containing observations
 #' @param cluster a logical value indicating whether using the cluster option
 #'   while visualizing maps. Default: TRUE
+#' @param hover_column character with the name of the column to use for showing
+#'   location deployment information while hovering the mouse over. By default:
+#'   `deployment_id`. Use `NULL` to disable hovering.
 #'
-#' @importFrom dplyr distinct group_by count left_join select %>%
+#' @importFrom dplyr .data count distinct group_by left_join mutate one_of
+#'   select %>%
 #' @importFrom leaflet leaflet addTiles addCircleMarkers markerClusterOptions
 #' @importFrom glue glue
+#' @importFrom rlang !! sym
 #'
 #' @export
 #'
 #' @return a leaflet map
+#'
+#' @examples
+#' \dontrun {
+#' # cluster enabled and  `deployment_id` column for hovering
+#' visualize_species_per_deployment(camtrapdp$deployments,
+#'                                  camtrapdp$observations)
+#' # cluster disabled
+#' visualize_species_per_deployment(camtrapdp$deployments,
+#'                                  camtrapdp$observations,
+#'                                  cluster = FALSE)
+#' visualize_species_per_deployment(camtrapdp$deployments,
+#'                                  camtrapdp$observations,
+#'                                  hover_column = "location_name")
+#' }
 visualize_species_per_deployment <- function(deployments,
                                              observations,
-                                             cluster = TRUE) {
+                                             cluster = TRUE,
+                                             hover_column = "deployment_id") {
 
   # get species detected by each deployment
   species <-
     observations %>%
-    distinct(deployment_id, scientific_name)
+    distinct(.data$deployment_id, .data$scientific_name)
 
   # get amount of species detected by each deployment
   n_species <-
     species %>%
-    group_by(deployment_id) %>%
+    group_by(.data$deployment_id) %>%
     count()
 
   # get deployments with at least one observations, even if unidentified
@@ -56,38 +76,69 @@ visualize_species_per_deployment <- function(deployments,
   n_species <-
     n_species %>%
     left_join(deployments %>%
-      select(
-        deployment_id,
-        location_id,
-        location_name,
-        longitude,
-        latitude
+      select(one_of("deployment_id",
+                    "location_id", # optional field
+                    "location_name", # optional field
+                    "longitude",
+                    "latitude")
       ),
     by = "deployment_id"
     )
 
+  if (!is.null(hover_column)) {
+    n_species <-
+      n_species %>%
+      mutate(hover_info = !!sym(hover_column))
+  }
+
+  # make basic start map
   leaflet_map <-
     leaflet(n_species) %>%
     addTiles()
 
   if (cluster == TRUE) {
-    leaflet_map %>% addCircleMarkers(
-      lng = ~longitude,
-      lat = ~latitude,
-      radius = ~n,
-      stroke = FALSE,
-      fillOpacity = 0.5,
-      label = ~location_name,
-      clusterOptions = markerClusterOptions()
-    )
+    # cluster enabled
+    if (!is.null(hover_column)) {
+      # hovering enabled
+      leaflet_map %>% addCircleMarkers(
+        lng = ~longitude,
+        lat = ~latitude,
+        radius = ~n,
+        stroke = FALSE,
+        fillOpacity = 0.5,
+        label = ~hover_info,
+        clusterOptions = markerClusterOptions())
+    } else {
+      # hovering disabled
+      leaflet_map %>% addCircleMarkers(
+        lng = ~longitude,
+        lat = ~latitude,
+        radius = ~n,
+        stroke = FALSE,
+        fillOpacity = 0.5,
+        clusterOptions = markerClusterOptions())
+    }
   } else {
-    leaflet_map %>% addCircleMarkers(
-      lng = ~longitude,
-      lat = ~latitude,
-      radius = ~n,
-      stroke = FALSE,
-      fillOpacity = 0.5,
-      label = ~location_name
-    )
+    # cluster disabled
+    if (!is.null(hover_column)) {
+      # # hovering enabled
+      leaflet_map %>% addCircleMarkers(
+        lng = ~longitude,
+        lat = ~latitude,
+        radius = ~n,
+        stroke = FALSE,
+        fillOpacity = 0.5,
+        label = ~hover_info
+      )
+    } else {
+      # hovering disabled
+      leaflet_map %>% addCircleMarkers(
+        lng = ~longitude,
+        lat = ~latitude,
+        radius = ~n,
+        stroke = FALSE,
+        fillOpacity = 0.5
+      )
+    }
   }
 }

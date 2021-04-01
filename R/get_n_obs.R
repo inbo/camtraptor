888ad9053888487367ef18f08a6e25ec0055ae02
damianooldoni <damiano.oldoni@inbo.be>
2021-04-01 16:16:00 +0200

    Add species arg in get_obs() and update documentation

--- R/get_n_obs.R
+++ R/get_n_obs.R
@@ -1,17 +1,18 @@
 #' Get number of observations for each deployment
 #'
-#' Function to get the number of observations per deployment.
+#' Function to get the number of observations (of a subset of species) per
+#' deployment.
 #'
 #' @param datapkg a camera trap data package object, as returned by
 #'   `read_camtrap_dp()`, i.e. a list containing three data.frames:
 #'
-#' 1. `observations`
-#' 2. `deployments`
-#' 3. `multimedia`
+#'   1. `observations` 2. `deployments` 3. `multimedia`
 #'
-#' and a list with metadata: `datapackage`
+#'   and a list with metadata: `datapackage`
 #'
-#' @importFrom dplyr .data %>% bind_rows count group_by mutate select
+#' @param species a character with scientific name. If `NULL` (default) all
+#'   observations of all species are taken into account
+#'
 #' @importFrom dplyr .data %>% bind_rows count group_by mutate select ungroup
 #'
 #' @export
@@ -19,10 +20,28 @@
 #' @return a tibble (data.frame) with the following columns: - `deployment_id`
 #'   deployment unique identifier - `n`: (integer) number of observations
 #'
-get_n_obs <- function(datapkg) {
+#' @examples
+#' # get number of observations of all species (not identified individuals as well)
+#' get_n_obs(camtrapdp)
+#'
+#' # get number of observations of Gallinula chloropus
+#' get_n_obs(camtrapdp, species = "Gallinula chloropus")
+#'
+get_n_obs <- function(datapkg, species = NULL) {
 
   # check input data package
   check_datapkg(datapkg)
+
+  # check species
+  valid_species <- get_species(datapkg)
+  check_value(species, valid_species, "species")
+
+  # select observations with the selected species
+  if (!is.null(species)) {
+    datapkg$observations <-
+      datapkg$observations %>%
+      filter(.data$scientific_name == species)
+  }
 
   # extract observations and deployments
   observations <- datapkg$observations
@@ -44,7 +63,7 @@ get_n_obs <- function(datapkg) {
     select(.data$deployment_id) %>%
     mutate(n = 0)
 
-  # add them to n_species and return df with column n as integer
+  # add them to n and return df with column n as integer
   n_obs %>%
     bind_rows(deployments_no_obs) %>%
     mutate(n = as.integer(.data$n))

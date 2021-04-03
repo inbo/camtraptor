d71f187c075f16bb0b83b157d8d71bfcd92a0204
damianooldoni <damiano.oldoni@inbo.be>
2021-04-03 21:47:50 +0200

    Configure tests and write first tests get_rai

---
+++ tests/testthat/test-get_rai.R
@@ -0,0 +1,21 @@
+test_that("get_rai returns the right dataframe", {
+
+  output_anas_platyrhyncos <- get_rai(camtrapdp, "Anas platyrhynchos")
+
+  # type list
+  expect_type(get_rai(camtrapdp, "Anas platyrhynchos"), "list")
+
+  # class tibble data.frame
+  expect_equal(class(get_rai(camtrapdp, "Anas platyrhynchos")),
+              c("tbl_df", "tbl", "data.frame"))
+
+  # columns deployment_id and rai only
+  expect_equal(names(output_anas_platyrhyncos),
+               c("deployment_id",
+                 "rai"))
+})
+
+test_that("species is case insensitive", {
+  expect_equal(get_rai(camtrapdp, "Anas platyrhynchos"),
+               get_rai(camtrapdp, toupper("Anas platyrhynchos")))
+})

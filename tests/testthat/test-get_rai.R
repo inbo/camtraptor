45f96d5dfbf73079f2c84758771d9464803bab75
damianooldoni <damiano.oldoni@inbo.be>
2021-04-10 21:03:48 +0200

    Improve and add tests

--- tests/testthat/test-get_rai.R
+++ tests/testthat/test-get_rai.R
@@ -1,6 +1,6 @@
 test_that("get_rai returns error if no species is specified", {
 
-  expect_error(get_rai(camtrapdp))
+  expect_error(get_rai(camtrapdp, species = NULL))
   expect_error(get_rai(camtrapdp, species = character(0)))
 })
 
@@ -37,6 +37,25 @@ test_that("get_rai returns the right number of rows: all species selected", {
   # number of rows should be equal to number of species by number of deployments
   expect_equal(nrow(output_all_species),
                n_all_species * n_all_deployments)
+
+})
+
+test_that("get_rai returns the same if 'all' is used instead of vector with all species", {
+
+  all_species <- get_species(camtrapdp)
+  all_deployments <- unique(camtrapdp$deployments$deployment_id)
+
+  n_all_species <- nrow(all_species)
+  n_all_deployments <- length(all_deployments)
+
+  # calculate rai for all species using default "all" value
+  output_all_species_default <- get_rai(camtrapdp, species = "all")
+  # calculate rai for all species specifying the species
+  output_all_species <- get_rai(camtrapdp,
+                                species = all_species$scientific_name)
+
+  expect_equal(output_all_species, output_all_species_default)
+
 })
 
 test_that("species is case insensitive", {

8de4ca96bc2c2562eced393f1ab26bf0047b7d12
damianooldoni <damiano.oldoni@inbo.be>
2021-05-25 14:59:57 +0200

    Restore standard checks

--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -1,6 +1,6 @@
 test_that("camtrap dp data are structured correctly", {
   mica <- read_camtrap_dp(
-    "./inst/extdata/mica-minimal"
+    "./inst/extdata/mica-muskrat-and-coypu-20210302172233"
   )
   # slots are correctly named
   expect_true(all(names(mica) == c("datapackage",
@@ -16,4 +16,12 @@ test_that("camtrap dp data are structured correctly", {
   expect_true(is.data.frame(mica$multimedia))
   # observations is a df
   expect_true(is.data.frame(mica$observations))
+})
+
+test_that("multimedia are not loaded if flag multimedia is set to FALSE", {
+  # load data (multimedia is TRUE by default)
+  mica <- read_camtrap_dp("./inst/extdata/mica-muskrat-and-coypu-20210302172233",
+    multimedia = FALSE
+  )
+  expect_null(mica$multimedia)
 })

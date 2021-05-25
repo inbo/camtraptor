e9a22846282926bb54ffc1d723659eb1bf943255
damianooldoni <damiano.oldoni@inbo.be>
2021-05-25 13:23:07 +0200

    Try to avoid failure automatic tests by removing / in folder name

--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -1,6 +1,6 @@
 test_that("camtrap dp data are structured correctly", {
   mica <- read_camtrap_dp(
-    "./inst/extdata/mica-muskrat-and-coypu-20210302172233/"
+    "./inst/extdata/mica-muskrat-and-coypu-20210302172233"
   )
   # slots are correctly named
   expect_true(all(names(mica) == c("datapackage",
@@ -21,7 +21,7 @@ test_that("camtrap dp data are structured correctly", {
 test_that("multimedia are not loaded if flag multimedia is set to FALSE", {
   # load data (multimedia is TRUE by default)
   mica <- read_camtrap_dp(
-    "./inst/extdata/mica-muskrat-and-coypu-20210302172233/",
+    "./inst/extdata/mica-muskrat-and-coypu-20210302172233",
     multimedia = FALSE
   )
   expect_null(mica$multimedia)

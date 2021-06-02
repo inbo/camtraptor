3410b6f843343f7f441c9ccf9ab74647d027894d
damianooldoni <damiano.oldoni@inbo.be>
2021-06-02 14:59:17 +0200

    Add get_n_individuals and tests

---
+++ R/get_n_individuals.R
@@ -0,0 +1,174 @@
+#' Get number of individuals for each deployment
+#'
+#' Function to get the number of individuals (of a subset of species) per
+#' deployment. The number of observed individuals is stored in field `count` of
+#' `observations`.
+#'
+#' @param datapkg a camera trap data package object, as returned by
+#'   `read_camtrap_dp()`, i.e. a list containing three data.frames:
+#'
+#'   1. `observations` 2. `deployments` 3. `multimedia`
+#'
+#'   and a list with metadata: `datapackage`
+#'
+#' @param species a character with scientific names or common names (case
+#'   insensitive). If "all", default, all scientific names are automatically
+#'   selected. If `NULL` all observations of all species are taken into account
+#' @param sex a character defining the sex class to filter on, e.g. `"female"`
+#'   or `c("male", "undefined")`.  If `NULL`, default, all observations of all
+#'   sex classes are taken into account.
+#' @param age a character vector defining the age class to filter on, e.g.
+#'   `"adult"` or `c("subadult", "adult")`. If `NULL`, default, all observations
+#'   of all age classes are taken into account.
+#' @param ... filter predicates for filtering on deployments
+#' @importFrom dplyr .data %>% as_tibble bind_rows group_by count mutate
+#'   rename select summarise ungroup relocate
+#' @importFrom glue glue
+#' @export
+
+#' @return a tibble (data.frame) with the following columns:
+#' - `deployment_id`:  deployment unique identifier
+#' - `scientific_name`: scientific name of the species. This column is omitted
+#' if argument `species` = NULL
+#' - `n`: (integer) number of individuals
+#'
+#' @examples
+#'
+#' # get number of observations for each species
+#' get_n_individuals(camtrapdp)
+#'
+#' # get number of obs of all species, not identified individuals as well
+#' get_n_individuals(camtrapdp, species = NULL)
+#'
+#' # get number of observations of Gallinula chloropus
+#' get_n_individuals(camtrapdp, species = "Gallinula chloropus")
+#'
+#' # get number of observations of Common Moorhen
+#' get_n_individuals(camtrapdp, species = "Common Moorhen")
+#'
+#' # case insensitive
+#' get_n_individuals(camtrapdp, species = "cOmmon moorhEn")
+#'
+#' # specify age
+#' get_n_individuals(camtrapdp, age = "adult")
+#'
+#' # specify sex
+#' get_n_individuals(camtrapdp, sex = "female")
+#'
+#' # specify both sex and age
+#' get_n_individuals(camtrapdp, sex = "undefined", age = "adult")
+#'
+#' # applying filter(s), e.g. deployments with latitude >= 51.28
+#' get_n_individuals(camtrapdp, pred_gte("latitude", 51.28))
+#'
+get_n_individuals <- function(datapkg,
+                              ...,
+                              species = "all",
+                              sex = NULL,
+                              age = NULL) {
+  # check input data package
+  check_datapkg(datapkg)
+
+  # avoid to call variables like column names to make life easier using filter()
+  sex_value <- sex
+  age_value <- age
+
+
+  # check sex and age values
+  check_value(sex_value, unique(datapkg$observation$sex), "sex")
+  check_value(age_value, unique(datapkg$observation$age), "age")
+
+  # get observations of the selected species
+  if (!is.null(species)) {
+    # if species == all retrieve all detected species
+    if ("all" %in% species) {
+      # if also other values are present, they will be ignored
+      if (length(species) > 1) {
+        ignored_species <- species[!species == "all"]
+        warning(glue(
+          "Value 'all' found in species.
+              All others values are ignored: {ignored_species*}.",
+          .transformer = collapse_transformer(
+            sep = ", ",
+            last = " and "
+          )
+        ))
+      }
+      species <- get_species(datapkg)$scientific_name
+    }
+    # check species and get scientific names
+    species <- check_species(datapkg, species)
+    datapkg$observations <-
+      datapkg$observations %>%
+      filter(tolower(.data$scientific_name) %in% tolower(species))
+  }
+
+  # get observations of the specified sex
+  if (!is.null(sex)) {
+    datapkg$observations <-
+      datapkg$observations %>%
+      filter(sex %in% sex_value)
+  }
+
+  # get observations of the specified age
+  if (!is.null(age)) {
+    datapkg$observations <-
+      datapkg$observations %>%
+      filter(age %in% age_value)
+  }
+
+  # extract observations and deployments
+  observations <- datapkg$observations
+  deployments <- datapkg$deployments
+
+  # apply filtering
+  deployments <- apply_filter_predicate(
+    df = deployments,
+    verbose = TRUE,
+    ...)
+
+  deployment_id <- deployments$deployment_id
+
+  deployments_no_obs <- get_dep_no_obs(
+    datapkg,
+    pred_in("deployment_id",deployment_id)
+  )
+
+  # get number of individuals collected by each deployment for each species
+  n_individuals <-
+    observations %>%
+    group_by(.data$deployment_id,
+             .data$scientific_name) %>%
+    summarise(n = sum(.data$count)) %>%
+    ungroup()
+
+  # get all combinations deployments - scientific name
+  combinations_dep_species <-
+    expand.grid(deployments$deployment_id,
+                unique(c(observations$scientific_name, species))) %>%
+    rename(deployment_id = .data$Var1,
+           scientific_name = .data$Var2) %>%
+    as_tibble()
+
+  # set 0 to combinations without observed individuals (i.e. n = NA after join)
+  n_individuals <-
+    combinations_dep_species %>%
+    left_join(n_individuals,
+              by = c("deployment_id", "scientific_name")) %>%
+    mutate(n = ifelse(is.na(.data$n), 0, .data$n)) %>%
+    mutate(n = as.integer(.data$n))
+
+  if (is.null(species)) {
+    # sum all observations per deployment
+    n_individuals <-
+      n_individuals %>%
+      group_by(.data$deployment_id) %>%
+      summarise(n = sum(.data$n)) %>%
+      ungroup()
+  }
+
+  # order result by deployments and following same order as in deployments df
+  deployments %>%
+    select(deployment_id) %>%
+    left_join(n_individuals, by = "deployment_id")
+}
---
+++ tests/testthat/test-get_n_individuals.R
@@ -0,0 +1,242 @@
+test_that("get_n_individuals returns the right structure of dataframe", {
+
+  # species arg specified
+  output_anas_platyrhyncos <- get_n_individuals(camtrapdp,
+                                        species = "Anas platyrhynchos"
+  )
+
+  # type list
+  expect_type(output_anas_platyrhyncos, "list")
+
+  # class tibble data.frame
+  expect_equal(
+    class(output_anas_platyrhyncos),
+    c("tbl_df", "tbl", "data.frame")
+  )
+
+  # columns deployment_id, scientific_name and n
+  expect_equal(
+    names(output_anas_platyrhyncos),
+    c(
+      "deployment_id",
+      "scientific_name",
+      "n"
+    )
+  )
+
+  # species arg is NULL
+  output_general <- get_n_individuals(camtrapdp, species = NULL)
+
+  # columns deployment_id and n
+  expect_equal(
+    names(output_general),
+    c(
+      "deployment_id",
+      "n"
+    )
+  )
+})
+
+
+test_that("get_n_individuals returns the right number of rows: all species selected", {
+  all_species <- get_species(camtrapdp)
+  all_deployments <- unique(camtrapdp$deployments$deployment_id)
+
+  n_all_species <- nrow(all_species)
+  n_all_deployments <- length(all_deployments)
+
+  # calculate number of individuals for all species
+  output_all_species <- get_n_individuals(camtrapdp)
+
+  # number of rows should be equal to number of species by number of deployments
+  expect_equal(
+    nrow(output_all_species),
+    n_all_species * n_all_deployments
+  )
+})
+
+test_that(paste(
+  "get_n_individuals returns always the right number of rows:",
+  "species undetected in one deployment"
+), {
+  deployments <- unique(camtrapdp$deployments$deployment_id)
+
+  n_deployments <- length(deployments)
+
+  # calculate get_n_individuals for a species undetected in one deployment
+  output_ondatra_zibethicus <- get_n_individuals(camtrapdp,
+                                         species = "Ondatra zibethicus"
+  )
+
+  # number of rows should be equal to number of deployments
+  expect_equal(nrow(output_ondatra_zibethicus), n_deployments)
+})
+
+test_that(
+  "get_n_individuals returns rows ordered by the original order of deployments", {
+    # get the original order of deployment IDs
+    deployment_ids <- unique(camtrapdp$deployments$deployment_id)
+
+    # apply function
+    n_individuals <- get_n_individuals(camtrapdp)
+    deployments_in_n_individuals <- unique(n_individuals$deployment_id)
+    expect_equal(deployments_in_n_individuals, deployment_ids)
+  }
+)
+
+test_that("species = 'all' returns the same of using a vector with all species", {
+  all_species <- get_species(camtrapdp)
+  all_deployments <- unique(camtrapdp$deployments$deployment_id)
+
+  n_all_species <- nrow(all_species)
+  n_all_deployments <- length(all_deployments)
+
+  # calculate number of individuals for all species using default "all" value
+  output_all_species_default <- get_n_individuals(camtrapdp, species = "all")
+  # calculate number of individuals for all species specifying the species
+  output_all_species <- get_n_individuals(camtrapdp,
+                                  species = all_species$scientific_name
+  )
+
+  expect_equal(output_all_species, output_all_species_default)
+})
+
+test_that("species is case insensitive", {
+  expect_equal(
+    get_n_individuals(camtrapdp, species = "Anas platyrhynchos"),
+    get_n_individuals(camtrapdp, species = toupper("Anas platyrhynchos"))
+  )
+})
+
+test_that(paste(
+  "species accepts use of common names and return",
+  "the same as using scientic name"
+), {
+
+  # define scientific name
+  scn <- "Anas platyrhynchos"
+  # define correspondent vernacular name
+  vn <- "Mallard"
+
+  # get number of individuals for both cases
+  output_anas_platyrhyncos <- get_n_individuals(camtrapdp, species = scn)
+  output_mallard <- get_n_individuals(camtrapdp, species = vn)
+
+  # same outputs
+  expect_equal(output_anas_platyrhyncos, output_mallard)
+})
+
+test_that("if subset of species is specified, less individuals are returned", {
+  output_all_species <- get_n_individuals(camtrapdp)
+  output_anas_platyrhyncos <- get_n_individuals(camtrapdp,
+                                        species = "Anas platyrhynchos"
+  )
+
+  expect_true(sum(output_all_species$n) >= sum(output_anas_platyrhyncos$n))
+})
+
+test_that(paste(
+  "species is NULL returns an equal or higher number of",
+  "individuals than species = 'all'"
+), {
+  output_all_species_collapsed <- get_n_individuals(camtrapdp, species = NULL)
+  output_anas_platyrhyncos <- get_n_individuals(camtrapdp, species = "all") # default
+
+  expect_true(
+    sum(output_all_species_collapsed$n) >= sum(output_anas_platyrhyncos$n)
+  )
+})
+
+test_that("get_n_individuals returns a warning if 'all' is used with other values", {
+  all_species <- get_species(camtrapdp)
+
+  # use 'all' with other species
+  expect_warning(get_n_individuals(camtrapdp,
+                           species = c("all", all_species[1])
+  ))
+})
+
+test_that(paste(
+  "number of individuals is equal to sum of counts"
+), {
+  deploy_id <- "fff2f46e-8163-453c-9044-61fb77587f5d"
+  species <- "Anas platyrhynchos"
+  n_individuals_via_count <-
+    camtrapdp$observations %>%
+    filter(deployment_id == deploy_id) %>%
+    filter(scientific_name == species) %>%
+    pull(count) %>%
+    sum()
+  n_individuals <- get_n_individuals(camtrapdp,
+                     species = "Mallard",
+                     pred("deployment_id", deploy_id))
+  expect_equal(n_individuals$n, n_individuals_via_count)
+})
+
+test_that("sex filters data correctly", {
+  deploy_id <- "fff2f46e-8163-453c-9044-61fb77587f5d"
+  species <- "Anas platyrhynchos"
+  sex_value <- "female"
+  n_individuals_via_count <-
+    camtrapdp$observations %>%
+    filter(deployment_id == deploy_id) %>%
+    filter(scientific_name == species) %>%
+    filter(sex == sex_value) %>%
+    pull(count) %>%
+    sum()
+  n_individuals_females <- get_n_individuals(camtrapdp, species = NULL, sex = sex_value)
+  tot_n_individuals_females <- sum(n_individuals_females$n)
+  expect_equal(tot_n_individuals_females, n_individuals_via_count)
+  expect_equal(nrow(n_individuals_females), nrow(camtrapdp$deployments))
+})
+
+test_that("multiple sex values allowed", {
+  sex_value <- c("female", "undefined")
+  n_individuals_females_undefined_via_count <-
+    camtrapdp$observations %>%
+    filter(sex %in% sex_value) %>%
+    pull(count) %>%
+    sum()
+  n_individuals_females_undefined <- get_n_individuals(camtrapdp,
+                                                       species = NULL,
+                                                       sex = sex_value)
+  tot_n_individuals_females_undefined <- sum(n_individuals_females_undefined$n)
+  expect_equal(tot_n_individuals_females_undefined,
+               n_individuals_females_undefined_via_count)
+  expect_equal(nrow(n_individuals_females_undefined),
+               nrow(camtrapdp$deployments))
+})
+
+test_that("age filters data correctly", {
+  age_value <- "juvenile"
+  n_individuals_juvenile_via_count <-
+    camtrapdp$observations %>%
+    filter(age == age_value) %>%
+    pull(count) %>%
+    sum()
+  n_individuals_juvenile <- get_n_individuals(camtrapdp, species = NULL, age = age_value)
+  tot_n_individuals_juvenile <- sum(n_individuals_juvenile$n)
+  expect_equal(tot_n_individuals_juvenile, n_individuals_juvenile_via_count)
+  expect_equal(nrow(n_individuals_juvenile), nrow(camtrapdp$deployments))
+})
+
+test_that("multiple age values allowed", {
+  age_value <- c("juvenile", "adult")
+  n_individuals_juvenile_adult_via_count <-
+    camtrapdp$observations %>%
+    filter(age %in% age_value) %>%
+    pull(count) %>%
+    sum()
+  n_individuals_juvenile_adult <- get_n_individuals(camtrapdp,
+                                                    species = NULL,
+                                                    age = age_value)
+  tot_n_individuals_juvenile_adult <- sum(n_individuals_juvenile_adult$n)
+  expect_equal(tot_n_individuals_juvenile_adult,
+               n_individuals_juvenile_adult_via_count)
+  expect_equal(nrow(n_individuals_juvenile_adult), nrow(camtrapdp$deployments))
+})
+
+test_that("error returned if age or sex is not present", {
+  expect_error(get_n_individuals(camtrapdp, age = "bad"))
+  expect_error(get_n_individuals(camtrapdp, sex = "bad"))
+})

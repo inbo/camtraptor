d01a099e0ca604cb660bb8160656ab4d0366b266
damianooldoni <damiano.oldoni@inbo.be>
2021-06-04 21:10:47 +0200

    Rrfactot o tow distinct functions

--- R/get_rai.R
+++ R/get_rai.R
@@ -1,6 +1,9 @@
 #' Get Relative Abundance Index (RAI)
 #'
-#' Function to get the RAI (Relative Abundance Index) per deployment.
+#' @name get_rai
+#'
+#' @description Function to get the RAI (Relative Abundance Index) per
+#'   deployment.
 #'
 #' @param datapkg a camera trap data package object, as returned by
 #'   `read_camtrap_dp()`, i.e. a list containing three data.frames:
@@ -14,10 +17,6 @@
 #' @param species a character with scientific names or common names (case
 #'   insensitive). If "all" (default), all scientific names are automatically
 #'   selected
-#'   @param feature a character, `"n_obs"`, the default, or
-#'     `"n_individuals"`. If `"_obs"` is used, RAI is calculated using the
-#'     number of observations, while if `"n_individuals"` is used, RAI is
-#'     calculated using the number of detected individuals.
 #' @param sex a character defining the sex class to filter on, e.g. `"female"`
 #'   or `c("male", "undefined")`.  If `NULL`, default, all observations of all
 #'   sex classes are taken into account.
@@ -26,8 +25,6 @@
 #'   of all age classes are taken into account.
 #' @param ... filter predicates for filtering on deployments
 #'
-#' @importFrom dplyr .data %>% group_by left_join select summarise ungroup
-#'
 #' @export
 
 #' @return a tibble (data.frame) with the following columns:
@@ -35,15 +32,11 @@
 #' - `scientific_name` scientific name
 #' - `rai`: relative abundance index
 #'
-#' @examples
-#' # calculate RAI based on number of observations (default)
-#' get_rai(camtrapdp) # or
-#' get_rai(camtrapdp, feature = "n_obs")
-#' # calculate RAI based on number of individuals (default)
-#' get_rai(camtrapdp, feature = "n_individuals")
+#' @family RAI functions
 #'
-#' # all species
-#' get_rai(camtrapdp) # or
+#' @examples
+#' # calculate RAI for all species
+#' get_rai(camtrapdp) # species = "all" by default, so equivalent of
 #' get_rai(camtrapdp, species = "all")
 #'
 #' # selected species
@@ -70,22 +63,124 @@
 #' get_rai(camtrapdp, pred_gte("latitude", 51.28))
 #'
 get_rai <- function(datapkg, ...,
-                    feature = "n_obs",
                     species = "all",
                     sex = NULL,
                     age = NULL
                     ) {
+  get_rai_primitive(datapkg, ...,
+                    use = "n_obs",
+                    species,
+                    sex, age)
+}
 
+#' Get Relative Abundance Index (RAI) based on number of individuals
+#'
+#' @name get_rai_individuals
+#'
+#' @description Function to get the RAI (Relative Abundance Index) per
+#'   deployment based on number of detected individuals instead of the number of
+#'   observations.
+#'
+#' @param datapkg a camera trap data package object, as returned by
+#'   `read_camtrap_dp()`, i.e. a list containing three data.frames:
+#'
+#' 1. `observations`
+#' 2. `deployments`
+#' 3. `multimedia`
+#'
+#' and a list with metadata: `datapackage`
+#'
+#' @param species a character with scientific names or common names (case
+#'   insensitive). If "all" (default), all scientific names are automatically
+#'   selected
+#' @param sex a character defining the sex class to filter on, e.g. `"female"`
+#'   or `c("male", "undefined")`.  If `NULL`, default, all observations of all
+#'   sex classes are taken into account.
+#' @param age a character vector defining the age class to filter on, e.g.
+#'   `"adult"` or `c("subadult", "adult")`. If `NULL`, default, all observations
+#'   of all age classes are taken into account.
+#' @param ... filter predicates for filtering on deployments
+#'
+#' @export
+
+#' @return a tibble (data.frame) with the following columns:
+#' - `deployment_id` deployment unique identifier
+#' - `scientific_name` scientific name
+#' - `rai`: relative abundance index
+#'
+#' @family RAI functions
+#'
+#' @examples
+#' # calculate RAI based on number of individuals
+#' get_rai_individuals(camtrapdp) # species = "all" by default, so equivalent of
+#' get_rai_individuals(camtrapdp, species = "all")
+#'
+#' # selected species
+#' get_rai_individuals(camtrapdp,
+#'   species = c("Anas platyrhynchos", "Rattus norvegicus")
+#' )
+#'
+#' # with common names
+#' get_rai_individuals(camtrapdp, species = c("Mallard", "norway rat"))
+#'
+#' # mixed scientific and vernacular names
+#' get_rai_individuals(camtrapdp, species = c("Anas platyrhynchos", "norway
+#' rat"))
+#'
+#' # species argument is case insensitive
+#' get_rai_individuals(camtrapdp, species = c("ANAS plAtyRhynChOS"))
+#'
+#' # specify sex
+#' get_rai_individuals(camtrapdp, sex = "female")
+#' get_rai_individuals(camtrapdp, sex = c("female", "undefined"))
+#'
+#' # specify age
+#' get_rai_individuals(camtrapdp, age = "adult")
+#' get_rai_individuals(camtrapdp, age = c("adult", "subadult"))
+#'
+#' # apply filter(s): deployments with latitude >= 51.28
+#' get_rai_individuals(camtrapdp, pred_gte("latitude", 51.28))
+#'
+get_rai_individuals <- function(datapkg, ...,
+                    species = "all",
+                    sex = NULL,
+                    age = NULL
+) {
+  get_rai_primitive(datapkg, ...,
+                    use = "n_individuals",
+                    species = species,
+                    sex = sex, age = age)
+}
+
+
+#' Primitive function for RAI calculation
+#'
+#' This function is the primitive function behind `get_rai()` and
+#' `get_rai_individuals()` to calculate RAI based on number of observations or
+#' number of individuals respectivel
+#'
+#' @param use character, one of:
+#' - `"n_obs"`: calculate RAI based on number of observation (standard)
+#' - `"n_individuals"`: calculate RAI based on number of individuals
+#'
+#' @importFrom dplyr .data %>% group_by left_join select summarise ungroup
+#'
+#' @keywords internal
+#'
+#' @noRd
+#'
+#' @return a data.frame (tibble)
+get_rai_primitive <- function(datapkg, use, species, sex, age, ...) {
   # check input data package
   check_datapkg(datapkg)
 
   # define possible feature values
-  features <- c("n_obs", "n_individuals")
+  uses <- c("n_obs", "n_individuals")
 
-  # check feature
-  check_value(feature, features, "feature", null_allowed = FALSE)
-  assert_that(length(feature) == 1,
-              msg = "feature must have length 1")
+  # check use
+  check_value(use, uses, "use", null_allowed = FALSE)
+  assert_that(length(use) == 1,
+              msg = "use must have length 1")
 
   # get all identified species if species arg is equal to "all"
   if ("all" %in% species) {
@@ -94,12 +189,17 @@ get_rai <- function(datapkg, ...,
   # check species
   species <- check_species(datapkg, species)
 
-  if (feature == "n_obs") {
+  if (use == "n_obs") {
     # get number of observations
     n_df <- get_n_obs(datapkg, species = species, sex = sex, age = age, ...)
   } else {
     # get number of individuals
-    n_df <- get_n_individuals(datapkg, species = species, sex = sex, age = age, ...)
+    n_df <- get_n_individuals(datapkg,
+                              species = species,
+                              sex = sex,
+                              age = age,
+                              ...
+                              )
   }
 
   # extract deployments

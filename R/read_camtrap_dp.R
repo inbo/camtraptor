d66b8a90788f1006beb2fafe8c0fcf366fe0fda7
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-08-20 13:26:25 +0200

    Reuce length lines and remove unneeded comments in function

--- R/read_camtrap_dp.R
+++ R/read_camtrap_dp.R
@@ -1,10 +1,12 @@
 #' Read camtrap-dp formatted data
 #'
-#' This function reads camera trap data formatted following the [Camera Trap Data Package (Camtrap DP)](https://github.com/tdwg/camtrap-dp) format.
+#' This function reads camera trap data formatted following the [Camera Trap
+#' Data Package (Camtrap DP)](https://github.com/tdwg/camtrap-dp) format.
 #'
 #'
 #' @param path Path to the folder containing the camera trap data files.
-#' @param multimedia If `TRUE`, read multimedia records into memory. If `FALSE`, ignore multimedia file to speed up reading larger Camtrap DP packages.
+#' @param multimedia If `TRUE`, read multimedia records into memory. If `FALSE`,
+#'   ignore multimedia file to speed up reading larger Camtrap DP packages.
 #'
 #' @return A list of tibbles (dataframes).
 #'
@@ -19,20 +21,20 @@
 #' \dontrun{
 #' library(here)
 #' # Read Camtrap DP package
-#' camtrap_dp_dir <- here("inst", "extdata", "mica-muskrat-and-coypu-20210302172233")
+#' camtrap_dp_dir <- here(
+#'     "inst",
+#'     "extdata",
+#'     "mica-muskrat-and-coypu-20210302172233")
 #' muskrat_coypu <- read_camtrap_dp(camtrap_dp_dir)
 #'
 #' # Read Camtrap DP package and ignore multimedia file
-#' camtrap_dp_dir <- here("inst", "extdata", "gmu8-monitoring-faunabeheerzone-8-20210301093537")
+#' camtrap_dp_dir <- here(
+#'     "inst",
+#'     "extdata",
+#'     "gmu8-monitoring-faunabeheerzone-8-20210301093537")
 #' gmu8 <- read_camtrap_dp(camtrap_dp_dir, multimedia = FALSE)
 #' }
 read_camtrap_dp <- function(path, multimedia = TRUE) {
-  # add asserts to check presence of the 4 files
-  # TO BE DONE
-
-  # check uniqueness PKs (ids) to be sure no problems arise in other functions
-  # TO BE DONE
-
   # read files
   package <- read_package(file.path(path, "datapackage.json"))
   deployments <- read_resource(package, "deployments")

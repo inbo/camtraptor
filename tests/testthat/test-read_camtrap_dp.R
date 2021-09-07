e0c5bfde46111b8e08ddf299577807f6208aa2de
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-09-07 21:38:55 +0200

    Use expect_* in tests instead of assert_that

--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -26,7 +26,8 @@ test_that("output is a list of length 4", {
   dp_without_multimedia <- read_camtrap_dp(
     path = dp_path,
     multimedia = FALSE)
-  assert_that(length(dp_without_multimedia) == 4)
+  expect_equal(class(dp_without_multimedia), "list")
+  expect_equal(length(dp_without_multimedia), 4)
 })
 
 test_that("multimedia arg influences only slot multimedia", {
@@ -38,28 +39,24 @@ test_that("multimedia arg influences only slot multimedia", {
     path = dp_path,
     multimedia = FALSE)
   # multimedia is NULL only for dp_without_multimedia
-  assert_that(is.null(dp_without_multimedia$multimedia))
-  assert_that(!is.null(dp_with_multimedia$multimedia))
+  expect_null(dp_without_multimedia$multimedia)
+  expect_false(is.null(dp_with_multimedia$multimedia))
   # metadata are the same
-  assert_that(identical(dp_with_multimedia$metadata,
-                        dp_without_multimedia$metadata))
+  expect_identical(dp_with_multimedia$metadata,
+                        dp_without_multimedia$metadata)
   # observations are the same
-  assert_that(
-    identical(
-      # remove columns with NA only 
-      dp_with_multimedia$observations[colSums(!is.na(dp_with_multimedia$observations)) > 0],
-      # remove columns with NA only 
-      dp_without_multimedia$observations[colSums(!is.na(dp_without_multimedia$observations)) > 0]
-    )
+  expect_identical(
+    # remove columns with NA only 
+    dp_with_multimedia$observations[colSums(!is.na(dp_with_multimedia$observations)) > 0],
+    # remove columns with NA only 
+    dp_without_multimedia$observations[colSums(!is.na(dp_without_multimedia$observations)) > 0]
   )
   # deployments are the same
-  assert_that(
-    identical(
-      # remove columns with NA only 
-      dp_with_multimedia$deployments[colSums(!is.na(dp_with_multimedia$deployments)) > 0],
-      # remove columns with NA only 
-      dp_with_multimedia$deployments[colSums(!is.na(dp_with_multimedia$deployments)) > 0]
-    )
+  expect_identical(
+    # remove columns with NA only 
+    dp_with_multimedia$deployments[colSums(!is.na(dp_with_multimedia$deployments)) > 0],
+    # remove columns with NA only 
+    dp_with_multimedia$deployments[colSums(!is.na(dp_with_multimedia$deployments)) > 0]
   )
 })
 
@@ -68,7 +65,7 @@ test_that("Datapackage metadata is a list", {
   dp_without_multimedia <- read_camtrap_dp(
     path = dp_path,
     multimedia = FALSE)
-  assert_that(class(dp_without_multimedia$metadata) == "list")
+ expect_equal(class(dp_without_multimedia$metadata), "list")
 })
 
 test_that("Datapackage resources are named as in metadata$resource_names", {
@@ -77,7 +74,7 @@ test_that("Datapackage resources are named as in metadata$resource_names", {
     path = dp_path,
     multimedia = FALSE)
   resource_names <- dp_without_multimedia$metadata$resource_names
-  assert_that(all(resource_names %in% names(dp_without_multimedia)))
+  expect_true(all(resource_names %in% names(dp_without_multimedia)))
 })
 
 test_that("Datapackage resources are tibble dataframes", {
@@ -85,8 +82,8 @@ test_that("Datapackage resources are tibble dataframes", {
   dp_without_multimedia <- read_camtrap_dp(
     path = dp_path,
     multimedia = FALSE)
-  assert_that(all(c("tbl_df", "tbl", "data.frame") %in% 
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in% 
                 class(dp_without_multimedia$deployments)))
-  assert_that(all(c("tbl_df", "tbl", "data.frame") %in% 
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in% 
                 class(dp_without_multimedia$deployments)))
 })

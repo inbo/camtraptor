a0a3b9d9510a388acb58a36f874b32f01cd83a84
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-09-08 20:53:28 +0200

    Work further on get_scientific_name
    Arise error if invalid vn is passed

--- R/get_scientific_name.R
+++ R/get_scientific_name.R
@@ -1,7 +1,12 @@
 #' Get scientific name based on its input_vernacular name
 #'
-#' This function returns the scientific name(s) of a vector of input_vernacular names
-#' based on the observations table of the given camera trap data package.
+#' This function returns the scientific name(s) of a vector of input_vernacular
+#' names based on the taxonomic information in metadata slot `taxonomic` of the
+#' given  camera trap data package.
+#' 
+#' The match is performed case insensitively.
+#' 
+#' If a vernacular name is not valid, an error is returned
 #'
 #' @param datapkg a camera trap data package object, as returned by
 #'   `read_camtrap_dp()`, i.e. a list containing three data.frames:
@@ -20,17 +25,31 @@
 #' @return a character vector of scientific name(s)
 #'
 #' @examples
+#' # one or more vernacular names
+#' get_scientific_name(camtrapdp, "brown rat")
 #' get_scientific_name(camtrapdp, c("brown rat", "mallard"))
 #' # vernacular names can be passed in different languages 
 #' get_scientific_name(camtrapdp, c("brown rat", "wilde eend"))
 #' # the search is performed case insensitively
 #' get_scientific_name(camtrapdp, c("BrOwN RaT"))
-#' # if scientific names are passed, they are returned in the case correct form
-#' get_scientific_name(camtrapdp, c("Rattus norvegicus", "MyOcAsToR coYPuS"))
+#' # error is returned if at least one invalid vernacular name is passed
+#' \dontrun{
+#' get_scientfic_name(camtrapdp, "this is a bad vernacular name")
+#' # a scientific name is an invalid vernacular name of course
+#' get_scientific_name(camtrapdp, c("Rattus norvegicus", "wilde eend"))
+#' }
 get_scientific_name <- function(datapkg, vernacular_name) {
 
   all_sn_vn <- get_species(datapkg) # datapkg check happens in get_species
 
+  # get vernacular names for check
+  all_vn <- 
+    all_sn_vn %>%
+    select(starts_with("vernacular_name"))
+  # check validity verncular_name argument
+  check_value(arg = tolower(vernacular_name), 
+              options = unlist(all_vn) %>% tolower(), 
+              arg_name = "vernacular_name", null_allowed = FALSE)
   
   input_vernacular <- vernacular_name
 
@@ -41,26 +60,16 @@ get_scientific_name <- function(datapkg, vernacular_name) {
   map_chr(
     input_vernacular, 
     function(v) {
-      # if a scientific name is given (or any vErSiOn of it), return it with the
-      # right
-      if (tolower(v) %in% tolower(all_sn_vn$scientific_name)) {
-        sc_n <- 
-          all_sn_vn %>%
-          filter(tolower(.data$scientific_name) == tolower(v)) %>%
-          pull(.data$scientific_name)
-        sc_n
-      } else {
-        # search within the columns with vernacular names
-        sc_n <- 
-          all_sn_vn %>%
-          filter(if_any(starts_with("vernacular_name"),
-                        ~ tolower(.) %in% tolower(v))) %>%
-          pull(.data$scientific_name)
-        if (length(sc_n) == 0) {
-          message(glue("{v} not found"))
-          sc_n <- NA_character_
-        }
-        sc_n
+      # search within the columns with vernacular names
+      sc_n <- 
+        all_sn_vn %>%
+        filter(if_any(starts_with("vernacular_name"),
+                      ~ tolower(.) %in% tolower(v))) %>%
+        pull(.data$scientific_name)
+      if (length(sc_n) == 0) {
+        message(glue("{v} is not a valid vernacular name."))
+        sc_n <- NA_character_
       }
+      sc_n
     })
 }
--- tests/testthat/test-get_scientific_name.R
+++ tests/testthat/test-get_scientific_name.R
@@ -1,6 +1,7 @@
-test_that("If a vernacular names is not found, NA is returned", {
-  sc_names <- get_scientific_name(camtrapdp, c("brown rat", "quaqua"))
-  expect_equal(sc_names, c("Rattus norvegicus", NA_character_))
+test_that("If a vernacular names is not found, error arises", {
+  expect_error(get_scientific_name(camtrapdp, c("brown rat", "quaqua")))
+  # a scientific name is also an invalid input for vernacular_name
+  expect_error(get_scientific_name(camtrapdp, "Rattus norvegicus"))
 })
 
 test_that("Functions works well with multiple vernacular names", {
@@ -15,10 +16,5 @@ test_that("Functions works well with vernacular names of different languages", {
 
 test_that("Functions works case insensitively", {
   sc_names <- get_scientific_name(camtrapdp, c("BrOwN RaT"))
-  expect_equal(sc_names, c("Rattus norvegicus"))
-})
-
-test_that("scientific names are returned in the case correct form if passed", {
-  sc_names <- get_scientific_name(camtrapdp, c("Rattus NORVEGICUS"))
   expect_equal(sc_names, c("Rattus norvegicus"))
 })

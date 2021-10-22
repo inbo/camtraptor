7e6cc068ec238903b83968609328fb8ba92d535e
peterdesmet <peter.desmet@inbo.be>
2021-10-22 15:04:40 +0200

    Rebuild camtrapdp.rda for tests
    Creates error on devtools::document() @damianooldoni

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Sample of camtrap-dp formatted data
 #'
-#' A sample camera trap data package of musk rat and coypu from MICA project.
+#' A sample camera trap data package of muskrat and coypu from MICA project.
 #'
 #' @source https://github.com/tdwg/dwc-for-biologging/tree/403f57db105982dc05b70f3cf66fd2b5591798db/derived/camtrap-dp/data/raw
 "camtrapdp"

5d8f794e9c6ebc885a688728c80a164c70d0bab3
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-11-18 15:27:48 +0100

    Update test-get_scientific_name.R

--- tests/testthat/test-get_scientific_name.R
+++ tests/testthat/test-get_scientific_name.R
@@ -1,20 +1,20 @@
 test_that("If a vernacular names is not found, error arises", {
-  expect_error(get_scientific_name(mica, c("brown rat", "quaqua")))
+  expect_error(get_scientific_name(mica, c("mallard", "quaqua")))
   # a scientific name is also an invalid input for vernacular_name
-  expect_error(get_scientific_name(mica, "Rattus norvegicus"))
+  expect_error(get_scientific_name(mica, "Anas platyrhynchos"))
 })
 
 test_that("Functions works well with multiple vernacular names", {
-  sc_names <- get_scientific_name(mica, c("brown rat", "mallard"))
-  expect_equal(sc_names, c("Rattus norvegicus", "Anas platyrhynchos"))
+  sc_names <- get_scientific_name(mica, c("beech marten", "mallard"))
+  expect_equal(sc_names, c("Martes foina", "Anas platyrhynchos"))
 })
 
 test_that("Functions works well with vernacular names of different languages", {
-  sc_names <- get_scientific_name(mica, c("brown rat", "wilde eend"))
-  expect_equal(sc_names, c("Rattus norvegicus", "Anas platyrhynchos"))
+  sc_names <- get_scientific_name(mica, c("beech marten", "wilde eend"))
+  expect_equal(sc_names, c("Martes foina", "Anas platyrhynchos"))
 })
 
 test_that("Functions works case insensitively", {
-  sc_names <- get_scientific_name(mica, c("BrOwN RaT"))
-  expect_equal(sc_names, c("Rattus norvegicus"))
+  sc_names <- get_scientific_name(mica, c("beeCH MArten"))
+  expect_equal(sc_names, c("Martes foina"))
 })

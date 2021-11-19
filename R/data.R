08613942385ac1acc68a8bece87e13bb88bd63af
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-11-19 16:09:09 +0100

    Mention the bigger datapkg in extdata in documentation mica

--- R/data.R
+++ R/data.R
@@ -1,6 +1,11 @@
 #' Sample of camtrap-dp formatted data
 #'
 #' A sample camera trap data package, based on data from the MICA project. Also stored in `inst`.
+#' 
+#' This sample is an extract from a bigger camera trap data package saved as raw
+#' data in `inst/extdata/mica_zenodo_5590881`. For memory reasons `media.csv`
+#' resource is not present. The entire data package can be downloaded from
+#' [zenodo](https://zenodo.org/record/5590881).
 #'
 #' @source https://github.com/tdwg/camtrap-dp/tree/ad0278ef86ef518dacfb306c598dce97667cfb81/example
 "mica"

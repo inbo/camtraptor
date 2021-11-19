05e5eb310032df4953a0b4af859015fa77b1d6af
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-11-19 15:51:07 +0100

    Solve #51
    and update to new format (camelCase)

--- tests/testthat/test-get_species.R
+++ tests/testthat/test-get_species.R
@@ -18,3 +18,51 @@ test_that("right (number of) species", {
     )
   )
 })
+
+test_that("function works fine with missing vernacular name slots", {
+  taxonomy <- list(
+    list(scientificName = "Martes foina",
+         vernacularNames = list(en = "aa", nl = "bb")),
+    # missing Dutch vernacular name
+    list(scientificName = "Ardea cinerea",
+         vernacularNames = list(en = "cc")),
+    # missing vernacular names
+    list(scientificName = "Anas strepera", vernacularNames = list())
+  )
+  mica_modified <- mica
+  mica_modified$datapackage$taxonomic <- taxonomy
+  species_df <- get_species(mica_modified)
+  # number of rows = number of species
+  expect_equal(nrow(species_df), length(mica_modified$datapackage$taxonomic))
+  # number of columns = number of slots of species list + vernacular names - 1
+  expect_equal(
+    ncol(species_df),
+    length(taxonomy[[1]]) + length(taxonomy[[1]]$vernacularNames) - 1
+  )
+  # column names
+  expect_equal(
+    names(species_df), 
+    c("scientificName",
+      paste("vernacularNames", names(taxonomy[[1]]$vernacularNames),sep = ".")
+    )
+  )
+  # empty slots are converted to NA
+  # Dutch vernacular name of Ardea cinerea not present
+  expect_true(
+    is.na(species_df %>%
+            filter(scientificName == "Ardea cinerea") %>%
+            pull(vernacularNames.nl))
+  )
+  # English vernacular name of Anas strepera not present
+  expect_true(
+    is.na(species_df %>%
+            filter(scientificName == "Anas strepera") %>%
+            pull(vernacularNames.en))
+  )
+  # Dutch vernacular name of Anas strepera not present
+  expect_true(
+    is.na(species_df %>%
+            filter(scientificName == "Anas strepera") %>%
+            pull(vernacularNames.nl))
+  )
+})

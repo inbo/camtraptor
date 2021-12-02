1f01021d615996754ec32fe9c84efc0278ad7dec
LynnINBO <72390052+LynnINBO@users.noreply.github.com>
2021-12-02 15:49:19 +0100

    test with nphk
    #70

--- R/get_custom_effort.R
+++ R/get_custom_effort.R
@@ -38,3 +38,39 @@ effort1 <- dailyOps1 %>%
   arrange(y, m) %>%
   mutate(trapEffort_hours = trapEffort_days * 24)
 
+## For Hoge Kempen #############################################################
+
+# test df from Agouti for Florian
+nphk <- read.delim("./tests/nphk_deployments.csv",
+                   sep=",")
+# parse dates
+nphk <- nphk %>%
+  mutate(start = ymd_hms(start, tz="UTC"),
+         end = ymd_hms(end, tz="UTC")) %>% # JIM CHECK DATES THAT DON'T PARSE (7)
+  filter(!is.na(start)) %>% # unparsed dates give NA
+  filter(!is.na(end)) # same
+
+# using camtrapR to get operations matrix
+operations2 <- cameraOperation(nphk, 
+                              stationCol = "deploymentID", 
+                              setupCol = "start", 
+                              retrievalCol = "end")
+# Adding up the number of cams active each day
+dailyOps2 <- colSums(operations2, na.rm = T, dims = 1) # ouput : vector
+# change vector to df
+dailyOps2 <- data.frame(date = names(dailyOps2),
+                       ncams = dailyOps2,
+                       row.names = NULL)
+# parse dates again, extract month and year
+dailyOps2 <- dailyOps2 %>%
+  mutate(date = ymd(date)) %>%
+  mutate(m = month(date, label=T, abbr=F),
+         y = year(date))
+# tally by month/year for effort
+effort2 <- dailyOps2 %>%
+  group_by(m, y) %>%
+  tally(ncams, name="trapEffort_days") %>%
+  ungroup() %>%
+  arrange(y, m) %>%
+  mutate(trapEffort_hours = trapEffort_days * 24)
+

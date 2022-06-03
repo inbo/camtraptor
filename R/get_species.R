50a1243e4f2177ddd8795fc592db8e49b3bfde18
peterdesmet <peter.desmet@inbo.be>
2022-06-03 08:52:16 +0200

    Minor edits to get_species()

--- R/get_species.R
+++ R/get_species.R
@@ -14,19 +14,17 @@
 #'
 #' @examples
 #' get_species(mica)
-#'
 get_species <- function(datapkg) {
-
-  # check input data package
+  # Check input data package
   check_datapkg(datapkg)
-  
-  # get vernacular names and scientific names from datapackage (taxonomic
-  # slot)
-  if (!"taxonomic" %in% names(datapkg$datapackage)) return(NULL)
-  else {
+
+  # Get taxonomic information from package metadata
+  if (!"taxonomic" %in% names(datapkg$datapackage)) {
+    return(NULL)
+  } else {
     taxonomy <- datapkg$datapackage$taxonomic
     if ("vernacularNames" %in% names(taxonomy[[1]])) {
-      # get all languages used in vernacularNames
+      # Get all languages used in vernacularNames
       langs <- map(taxonomy, function(x) {
         vernacular_languages <- NULL
         if ("vernacularNames" %in% names(x)) {
@@ -34,20 +32,18 @@ get_species <- function(datapkg) {
         }
       })
       langs <- unique(unlist(langs))
-      
-      # fill empty vernacular names with NA
-      taxonomy <- map(taxonomy,
-                      function(x) {
-                        missing_langs <- langs[!langs %in% names(x$vernacularNames)]
-                        for (i in missing_langs) {
-                          x$vernacularNames[[i]] <- NA_character_
-                        }
-                        x
-                      })
+
+      # Fill empty vernacular names with NA
+      taxonomy <- map(taxonomy, function(x) {
+        missing_langs <- langs[!langs %in% names(x$vernacularNames)]
+        for (i in missing_langs) {
+          x$vernacularNames[[i]] <- NA_character_
+        }
+        x
+      })
     }
-    map_dfr(
-      taxonomy,
-      function(x) x %>% as.data.frame()) %>% 
-      tibble()
+    map_dfr(taxonomy, function(x) {
+      tibble(as.data.frame(x))
+    })
   }
 }

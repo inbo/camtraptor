dc95dfca0a2b4ce75a606aecbcb4eaa022901e56
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-06-13 10:55:04 +0200

    Add $data to some examples

--- R/read_camtrap_dp.R
+++ R/read_camtrap_dp.R
@@ -44,9 +44,9 @@
 #'   package = "camtraptor"
 #' )
 #' muskrat_coypu_with_issues <- read_camtrap_dp(camtrap_dp_file_with_issues, media = TRUE)
-#' readr::problems(muskrat_coypu_with_issues$deployments)
-#' readr::problems(muskrat_coypu_with_issues$observations)
-#' readr::problems(muskrat_coypu_with_issues$media)
+#' readr::problems(muskrat_coypu_with_issues$data$deployments)
+#' readr::problems(muskrat_coypu_with_issues$data$observations)
+#' readr::problems(muskrat_coypu_with_issues$data$media)
 #' }
 read_camtrap_dp <- function(file = NULL,
                             media = TRUE,

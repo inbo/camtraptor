0eda223c2e0cb85b88bc49a19867ad3860001425
rafnuss <rafnuss@gmail.com>
2022-08-22 16:28:30 -0400

    change write_dwc_wi to read_wi which convert automatically to the package format

---
+++ tests/testthat/test-read_wi.R
@@ -0,0 +1,6 @@
+directory <- "tests/testthat/wi_export_sample/"
+test_that("file is checked properly", {
+  read_wi(directory = directory)
+})
+
+
--- tests/testthat/test-write_dwc_wi.R
+++
@@ -1,5 +0,0 @@
-import_directory <- "wi_export_sample/"
-
-test_that("file is checked properly", {
-  write_dwc_wi(import_directory = import_directory, export_directory = tempdir())
-})

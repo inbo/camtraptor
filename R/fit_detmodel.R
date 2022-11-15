b4562757eabd8b25f00042348302714419872ad9
MarcusRowcliffe <marcus.rowcliffe@ioz.ac.uk>
2022-11-15 12:05:27 +0000

    Import Working fit_detmodel function

--- R/fit_detmodel.R
+++ R/fit_detmodel.R
@@ -3,6 +3,8 @@
 #' Fits a detection function to a data package and estimates effective 
 #' detection distance (EDD).
 #' 
+#' @param formula A two sided formula relating distance or angle data 
+#'   to covariates.
 #' @param package Camera trap data package object, as returned by
 #'   `read_camtrap_dp()`.
 #' @param species A character string indicating species subset to analyse; user
@@ -13,15 +15,62 @@
 #' @seealso \code{\link{Distance::ds}}
 #' @family density estimation functions
 #' @export
-fit_detmodel <- function(package,
-                         species=NULL){
+fit_detmodel <- function(formula, 
+                         package, 
+                         species=NULL, 
+                         newdata=NULL, ...){
+  
+  # get and check model variables
+  allvars <- all.vars(formula)
+  depvar <- allvars[1]
+  covars <- tail(allvars, -1)
+  data <- package$data$observations
+  if(!all(allvars %in% names(data))) stop("Can't find all model variables in data")
+  if("distance" %in% covars) stop("Cannot use \"distance\" as a covariate name - rename and try again")
+  
+  # set up data
   if(is.null(species)) species <- select_species(package)
-  data <- package$data$observations %>%
+  data <- data %>%
     subset(scientificName==species) %>%
     dplyr::select(all_of(allvars)) %>%
     tidyr::drop_na() %>%
     as.data.frame()
   if("useDeployment" %in% names(data)) data <- subset(data, useDeployment)
   
+  classes <- dplyr::summarise_all(data, class)
+  if(classes[depvar]=="numeric"){
+    data <- data %>%
+      dplyr::rename(distance=all_of(depvar)) %>%
+      dplyr::mutate(distance=abs(distance))
+  } else{
+    cats <- strsplit(as.character(dplyr::pull(data, depvar)), "-")
+    data$distbegin <- unlist(lapply(cats, function(x) as.numeric(x[1])))
+    data$distend <- unlist(lapply(cats, function(x) as.numeric(x[2])))
+    data$distance <- (data$distbegin + data$distend) / 2
+  }
+  
+  # model fitting
+  args <- c(data=list(data), formula=formula[-2], list(...))
+  mod <- suppressWarnings(suppressMessages(do.call(ds, args)$ddf))
+  
+  # esw prediction
+  if(length(covars)==0) 
+    newdata <- data.frame(x=0) else{
+      if(is.null(newdata)){
+        newdata <- data %>% dplyr::select(all_of(covars)) %>%
+          lapply(function(x) 
+            if(is.numeric(x)) mean(x, na.rm=T) else sort(unique(x)))  %>%
+          expand.grid()
+      } else{
+        if(!all(covars %in% names(newdata))) stop("Can't find all model covariates in newdata")
+      }}
+  prdn <- predict(mod, newdata, esw=TRUE, se.fit=TRUE)
+  if(mod$meta.data$point){
+    prdn$se.fit <- 0.5 * prdn$se.fit / (pi * prdn$fitted)^0.5
+    prdn$fitted <- sqrt(prdn$fitted/pi)
+  }
+  ed <- cbind(estimate=prdn$fitted, se=prdn$se.fit)
+  if(length(covars)>=1) ed <- cbind(newdata, ed)
+  mod$edd <- ed
+  mod
 }
-

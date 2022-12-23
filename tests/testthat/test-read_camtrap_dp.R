c9a0109b54c3f7fc5e6bb35207c83d4c3a5a3717
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-12-23 11:24:04 +0100

    Remove testthat::

--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -1,27 +1,27 @@
-testthat::test_that("file is checked properly", {
-  testthat::expect_error(read_camtrap_dp("aaa"))
-  testthat::expect_error(read_camtrap_dp(1))
+test_that("file is checked properly", {
+  expect_error(read_camtrap_dp("aaa"))
+  expect_error(read_camtrap_dp(1))
 })
 
-testthat::test_that("test warnings", {
+test_that("test warnings", {
   local_edition(2)
   camtrap_dp_file_with_issues <- system.file("extdata", "mica_parsing_issues", "datapackage_for_parsing_issues.json", package = "camtraptor")
   # deployments
-  testthat::expect_warning(
+  expect_warning(
     camtraptor::read_camtrap_dp(
       file = camtrap_dp_file_with_issues
     ),
     "One or more parsing issues occurred while reading deployments."
   )
   # observations
-  testthat::expect_warning(
+  expect_warning(
     camtraptor::read_camtrap_dp(
       file = camtrap_dp_file_with_issues
     ),
     "One or more parsing issues occurred while reading observations."
   )
   # media
-  testthat::expect_warning(
+  expect_warning(
     camtraptor::read_camtrap_dp(
       file = camtrap_dp_file_with_issues
     ),
@@ -30,17 +30,17 @@ testthat::test_that("test warnings", {
 })
 
 
-testthat::test_that("media is checked properly", {
+test_that("media is checked properly", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
-  testthat::expect_error(read_camtrap_dp(
+  expect_error(read_camtrap_dp(
     file = dp_path,
     media = "must_be_a_logical!"
   ))
 })
 
-testthat::test_that("output is a list", {
+test_that("output is a list", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -48,11 +48,11 @@ testthat::test_that("output is a list", {
     file = dp_path,
     media = FALSE
   )
-  testthat::expect_true(is.list(dp_without_media))
-  testthat::expect_equal(class(dp_without_media), "list")
+  expect_true(is.list(dp_without_media))
+  expect_equal(class(dp_without_media), "list")
 })
 
-testthat::test_that("output data slot is a list of length 3", {
+test_that("output data slot is a list of length 3", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -60,11 +60,11 @@ testthat::test_that("output data slot is a list of length 3", {
     file = dp_path,
     media = FALSE
   )
-  testthat::expect_true("data" %in% names(dp_without_media))
-  testthat::expect_equal(length(dp_without_media$data), 3)
+  expect_true("data" %in% names(dp_without_media))
+  expect_equal(length(dp_without_media$data), 3)
 })
 
-testthat::test_that("media arg influences only slot media", {
+test_that("media arg influences only slot media", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -77,16 +77,16 @@ testthat::test_that("media arg influences only slot media", {
     media = FALSE
   )
   # media is NULL only for dp_without_media
-  testthat::expect_null(dp_without_media$data$media)
-  testthat::expect_false(is.null(dp_with_media$data$media))
+  expect_null(dp_without_media$data$media)
+  expect_false(is.null(dp_with_media$data$media))
   # metadata are the same
   metadata_with_media <- dp_with_media
   metadata_with_media$data <- NULL
   metadata_without_media <- dp_without_media
   metadata_without_media$data <- NULL
-  testthat::expect_identical(metadata_with_media, metadata_without_media)
+  expect_identical(metadata_with_media, metadata_without_media)
   # observations are the same
-  testthat::expect_identical(
+  expect_identical(
     # remove columns with NA only
     dp_with_media$data$observations[colSums(
       !is.na(dp_with_media$data$observations)
@@ -97,7 +97,7 @@ testthat::test_that("media arg influences only slot media", {
     ) > 0]
   )
   # deployments are the same
-  testthat::expect_identical(
+  expect_identical(
     # remove columns with NA only
     dp_with_media$data$deployments[colSums(
       !is.na(dp_with_media$data$deployments)
@@ -109,7 +109,7 @@ testthat::test_that("media arg influences only slot media", {
   )
 })
 
-testthat::test_that("Datapackage resources are named as in resource_names", {
+test_that("Datapackage resources are named as in resource_names", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -118,10 +118,10 @@ testthat::test_that("Datapackage resources are named as in resource_names", {
     media = FALSE
   )
   resource_names <- dp_without_media$resource_names
-  testthat::expect_true(all(resource_names %in% names(dp_without_media$data)))
+  expect_true(all(resource_names %in% names(dp_without_media$data)))
 })
 
-testthat::test_that("Datapackage resources are tibble dataframes", {
+test_that("Datapackage resources are tibble dataframes", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -129,13 +129,13 @@ testthat::test_that("Datapackage resources are tibble dataframes", {
     file = dp_path,
     media = FALSE
   )
-  testthat::expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
     class(dp_without_media$data$deployments)))
-  testthat::expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
     class(dp_without_media$data$observations)))
 })
 
-testthat::test_that("sc. names and vernacular names in obs match the info in taxonomic slot", {
+test_that("sc. names and vernacular names in obs match the info in taxonomic slot", {
   dp_path <- system.file("extdata", "mica", "datapackage.json",
     package = "camtraptor"
   )
@@ -148,54 +148,54 @@ testthat::test_that("sc. names and vernacular names in obs match the info in tax
     function(x) x %>% as.data.frame()
   ) %>%
     dplyr::tibble()
-  testthat::expect_true(all(names(taxon_infos) %in% names(dp$data$observations)))
+  expect_true(all(names(taxon_infos) %in% names(dp$data$observations)))
   # get scientific names from observations and check that they match with
   # taxonomic info
   sc_names <- dp$data$observations$scientificName[!is.na(
     dp$data$observations$scientificName
   )]
-  testthat::expect_true(all(sc_names %in% taxon_infos$scientificName))
+  expect_true(all(sc_names %in% taxon_infos$scientificName))
 
   # get vernacular names in English from observations and check that they match
   # with taxonomic info
   en_names <- dp$data$observations$vernacularNames.en[!is.na(
     dp$data$observations$vernacularNames.en
   )]
-  testthat::expect_true(all(en_names %in% taxon_infos$vernacularNames.en))
+  expect_true(all(en_names %in% taxon_infos$vernacularNames.en))
 
   # get vernacular names in Dutch from observations and check that they match
   # with taxonomic info
   nl_names <- dp$data$observations$vernacularNames.nl[!is.na(
     dp$data$observations$vernacularNames.nl
   )]
-  testthat::expect_true(all(nl_names %in% taxon_infos$vernacularNames.nl))
+  expect_true(all(nl_names %in% taxon_infos$vernacularNames.nl))
 })
 
-testthat::test_that("file can be an URL", {
+test_that("file can be an URL", {
   dp_path <- "https://raw.githubusercontent.com/tdwg/camtrap-dp/main/example/datapackage.json"
   dp <- read_camtrap_dp(
     file = dp_path,
     media = FALSE
   )
-  testthat::expect_true(is.list(dp))
-  testthat::expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
+  expect_true(is.list(dp))
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
     class(dp$data$deployments)))
-  testthat::expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
+  expect_true(all(c("tbl_df", "tbl", "data.frame") %in%
     class(dp$data$observations)))
 })
 
-testthat::test_that("path is deprecated", {
+test_that("path is deprecated", {
   dp_path_warning <- system.file("extdata", "mica", package = "camtraptor")
   rlang::with_options(
     lifecycle_verbosity = "warning",
-    testthat::expect_warning(read_camtrap_dp(
+    expect_warning(read_camtrap_dp(
       file = dp_path_warning,
       media = FALSE
     ))
   )
   rlang::with_options(
     lifecycle_verbosity = "warning",
-    testthat::expect_warning(read_camtrap_dp(
+    expect_warning(read_camtrap_dp(
       path = dp_path_warning,
       media = FALSE
     ))

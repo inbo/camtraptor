4af0239d9a272ba2ecaae85d323b91433d5bd24d
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-03-03 15:51:43 +0100

    add helper to add column to df, but only when it's missing

--- R/zzz.R
+++ R/zzz.R
@@ -376,3 +376,22 @@ add_unit_to_legend_title <- function(title, unit = NULL, use_brackets = TRUE) {
     paste(title, unit)
   }
 }
+
+#' Create columns, but only if they don't exist yet!
+#'
+#' Using dplyr::mutate(), add a new column, but only if it's missing
+#'
+#' @inherit dplyr::mutate
+#'
+#' @examples 
+#' # doesn't add a column when it already exists
+#' mutate_when_missing(cars, speed = "warp 9")
+#' # but does add a column when it doesn't exist yet
+#' mutate_when_missing(cars, space = "The final frontier")
+mutate_when_missing <- function(.data,...){
+  dots <- substitute(list(...))[-1]
+  cols_to_check <- names(sapply(dots, deparse))
+  columns_to_add <- cols_to_check[!cols_to_check %in% colnames(.data)]
+  if(!rlang::is_empty(columns_to_add)){.data <- dplyr::mutate(.data,...)}
+  return(.data)
+}

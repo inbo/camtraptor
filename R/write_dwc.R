a60b790206441e9f2482ae7017651cba1b7557b7
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-10 14:25:48 +0200

    change write_csv() to always use UNIX EOL characters

--- R/write_dwc.R
+++ R/write_dwc.R
@@ -273,7 +273,9 @@ write_dwc <- function(package, directory = ".") {
     if (!dir.exists(directory)) {
       dir.create(directory, recursive = TRUE)
     }
-    readr::write_csv(dwc_occurrence, dwc_occurrence_path, na = "")
-    readr::write_csv(dwc_audubon, dwc_audubon_path, na = "")
+    # Set end of line character to Unix style newlines, this way the function
+    # will always output the same files, regardless of OS
+    readr::write_csv(dwc_occurrence, dwc_occurrence_path, na = "", eol = "\n")
+    readr::write_csv(dwc_audubon, dwc_audubon_path, na = "", eol = "\n")
   }
 }

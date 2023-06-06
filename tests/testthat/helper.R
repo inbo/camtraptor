2390af69f1efa089e93bdabc568d6217c12bdd38
Peter Desmet <peter.desmet@inbo.be>
2023-06-06 12:10:27 +0200

    Update function description
    - Inherit parameters
    - Use newline for sentences
    - Rename which to file

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,21 +1,18 @@
-#' Helpers to output file paths
-#'
-#' `testthat::expect_snapshot_file()` needs a function to output the path of the
-#' file it needs to snapshot. This function is a wrapper of `write_dwc()` that
-#' outputs the path of either `dwc_occurrence.csv` or `dwc_audubon.csv`
-#'
-#' @param package A Camtrap DP, as read by `read_camtrap_dp()`.
-#' @param directory Character. The directory `write_dwc()` will write to.
-#' @param which Character. On of either `occurrence` or `audubon` to select
-#'   which of the paths of the two output files of `write_dwc()` to return
-#'
+#' Get snapshot for write_dwc()
+#' 
+#' Wrapper of `write_dwc()` that returns path of selected output file.
+#' Needed for `testthat::expect_snapshot_file()` which expects the path of a 
+#' single file to compare against snapshot.
+#' @inheritParams write_dwc
+#' @param file Either `occurrence` or `audubon` to select which output file of 
+#'   `write_dwc()` to return.
+#' @return Path of selected output file.
 #' @noRd
-#' @return the path of either `dwc_occurrence.csv` or `dwc_audubon.csv`
 #' @examples write_dwc_snapshot(mica, tempdir(), "occurrence")
-write_dwc_snapshot <- function(package, directory, which){
+write_dwc_snapshot <- function(package, directory, file){
   suppressMessages(write_dwc(package, directory))
   switch(
-    which,
+    file,
     occurrence = file.path(directory, "dwc_occurrence.csv"),
     audubon = file.path(directory, "dwc_audubon.csv")
   )

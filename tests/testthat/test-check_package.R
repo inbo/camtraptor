2121af87d8c22a3a1db2727114d8086adf07bc1d
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-07-14 14:24:38 +0200

    test for number/identity of data elements

--- tests/testthat/test-check_package.R
+++ tests/testthat/test-check_package.R
@@ -27,6 +27,23 @@ test_that("check_package() returns error on missing data", {
   )
 })
 
+test_that("check_package() returns error if not all elements are present", {
+  mica_no_media <- mica
+  mica_no_media$data$media <- NULL
+  expect_error(
+    check_package(mica_no_media),
+    regexp = "Can't find 1 elements in data package: media",
+    fixed = TRUE
+  )
+  mica_no_media_no_obs <- mica_no_media
+  mica_no_media_no_obs$data$observations <- NULL
+  expect_error(
+    check_package(mica_no_media_no_obs),
+    regexp = "Can't find 2 elements in data package: media and observations",
+    fixed = TRUE
+  )
+})
+
 
 test_that("check_package() returns TRUE on valid package", {
   expect_true(check_package(mica))

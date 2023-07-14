3f91881746f9496028eb4b0ef3b853d61673e324
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-07-14 10:44:34 +0200

    test that deployments(package) is identical to package$data$deployments

--- tests/testthat/test-deployments.R
+++ tests/testthat/test-deployments.R
@@ -1,4 +1,6 @@
 test_that("deployments() can extract deployments from a Camtrap DP", {
   expect_type(deployments(mica), "list")
   expect_s3_class(deployments(mica), "data.frame")
+  expect_identical(deployments(mica),
+                   mica$data$deployments)
 })

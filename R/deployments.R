7e949a4ec1286e02880b698d1878e4412f794986
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-07-14 10:45:22 +0200

    expand documentation

--- R/deployments.R
+++ R/deployments.R
@@ -1,6 +1,7 @@
 #' Retrieve deployment data from a Camera Trap Data Package
 #'
-#' @param package 
+#' @param package Camera trap data package object, as returned by
+#'   `read_camtrap_dp()`.
 #'
 #' @return A tibble with the deployments of the `package`
 #' @export

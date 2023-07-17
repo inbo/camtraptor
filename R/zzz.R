792868d625f1b3b6fdcbf48fd309701dc9c94462
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-07-17 15:30:50 +0200

    Relocate sequenceID in obs

--- R/zzz.R
+++ R/zzz.R
@@ -808,7 +808,10 @@ convert_observations_to_0.1.6 <- function(package, from = "1.0-rc.1") {
     observations <- observations %>%
       dplyr::mutate(sequenceID = NA)
   }
-  
+  observations <- dplyr::relocate(observations, 
+                                  "sequenceID", 
+                                  .after = "deploymentID"
+  )
   observations <- observations %>%
     dplyr::rename(timestamp = "eventStart")
   

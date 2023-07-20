a3fd5b042f701cf3c3c9cfaf169c97c8c36b9572
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-07-20 14:27:17 +0200

    use on.exit() to only clear tempdir() once.
    Instead of creating, clearing, filling, clearing. We are creating, and clearing when done.
    
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

--- tests/testthat/test-write_dwc.R
+++ tests/testthat/test-write_dwc.R
@@ -1,14 +1,12 @@
 test_that("write_dwc() writes csv files to a path", {
   out_dir <- file.path(tempdir(), "dwc")
-  unlink(out_dir, recursive = TRUE)
   dir.create(out_dir)
+  on.exit(unlink(out_dir, recursive = TRUE))
   suppressMessages(write_dwc(mica, directory = out_dir))
-
   expect_identical(
     list.files(out_dir, pattern = "*.csv"),
     c("dwc_audubon.csv", "dwc_occurrence.csv")
   )
-  unlink(out_dir, recursive = TRUE)
 })
 
 test_that("write_dwc() can return data as list of tibbles rather than files", {
@@ -21,14 +19,13 @@ test_that("write_dwc() can return data as list of tibbles rather than files", {
 })
 
 test_that("write_dwc() writes the expected meta.xml", {
-  out_dir <- file.path(tempdir(), "dwc")
-  unlink(out_dir, recursive = TRUE)
+  out_dir <- file.path(tempdir(), "dwc_meta")
   dir.create(out_dir)
+  on.exit(unlink(out_dir, recursive = TRUE))
   suppressMessages(write_dwc(mica, directory = out_dir))
   
   expect_true("meta.xml" %in% list.files(out_dir))
   expect_snapshot_file(file.path(out_dir, "meta.xml"))
-  unlink(out_dir, recursive = TRUE)
 })
 
 test_that("write_dwc() returns the expected Darwin Core terms as columns", {
@@ -93,18 +90,17 @@ test_that("write_dwc() returns the expected Darwin Core terms as columns", {
 })
 
 test_that("write_dwc() returns the expected Darwin Core mapping for a known dataset", {
-  out_dir <- file.path(tempdir(), "dwc")
-  unlink(out_dir, recursive = TRUE)
+  out_dir <- file.path(tempdir(), "dwc_mapping")
+  on.exit(unlink(out_dir, recursive = TRUE))
   
   # Use helper function that outputs path write_dwc() wrote to.  
   expect_snapshot_file(write_dwc_snapshot(mica, out_dir, "occurrence"))
   expect_snapshot_file(write_dwc_snapshot(mica, out_dir, "audubon"))
-  unlink(out_dir, recursive = TRUE)
 })
 
 test_that("write_dwc() returns files that comply with the info in meta.xml", {
   out_dir <- file.path(tempdir(), "dwc")
-  unlink(out_dir, recursive = TRUE)
+  on.exit(unlink(out_dir, recursive = TRUE))
   suppressMessages(write_dwc(mica, out_dir))
   
   # Test if all fields are present, in the right order
@@ -113,5 +109,4 @@ test_that("write_dwc() returns files that comply with the info in meta.xml", {
   # Test if the file locations (filenames) are the same as in meta.xml
   expect_location(file.path(out_dir,"dwc_occurrence.csv"))
   expect_location(file.path(out_dir,"dwc_audubon.csv"))
-  unlink(out_dir, recursive = TRUE)
 })

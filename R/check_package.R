3bc7a53d9f28931d804dea1ca3ff759a6f2bbd37
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-09-04 11:51:27 +0200

    Remove `media` arg from internal `check_package()`

--- R/check_package.R
+++ R/check_package.R
@@ -9,14 +9,11 @@
 #'
 #' @param package Camera trap data package
 #' @param datapkg Deprecated. Use `package` instead.
-#' @param media Has the `media` resource been loaded while reading the data
-#'   package? Default: `FALSE`.
 #' @return `TRUE` or error.
 #' @noRd
 check_package <- function(package = NULL,
                           datapkg = NULL,
-                          function_name,
-                          media = FALSE) {
+                          function_name) {
   if (lifecycle::is_present(datapkg) & !is.null(datapkg)) {
     lifecycle::deprecate_warn(
       when = "0.16.0",
@@ -27,11 +24,6 @@ check_package <- function(package = NULL,
       package <- datapkg
     }
   }
-  # check media arg
-  assertthat::assert_that(
-    media %in% c(TRUE, FALSE),
-    msg = "`media` must be a logical: TRUE or FALSE"
-  )
   # camera trap data package is a list
   assertthat::assert_that(is.list(package),
                           msg = "package is not a list.")
@@ -42,10 +34,7 @@ check_package <- function(package = NULL,
                           msg = "data element is missing from package")
   # check validity data element of package: does it contain deployments and
   # observations?
-  elements <- c("deployments", "observations")
-  if (media) {
-    elements <- c(elements, "media")
-  }
+  elements <- c("deployments", "media", "observations")
   tables_absent <- elements[
     !elements %in% names(package$data)
   ]
@@ -56,19 +45,10 @@ check_package <- function(package = NULL,
       .transformer = collapse_transformer(sep = ", ", last = " and ")
     )
   )
-  if (media) {
-    assertthat::assert_that(
-      !is.null(package$data$media),
-      msg = glue::glue("Can't find media in .$data.")
-    )
-  }
-  # check observations and deployments are data.frames
+  # check deployments, media and observations are data.frames
   assertthat::assert_that(is.data.frame(package$data$observations))
   assertthat::assert_that(is.data.frame(package$data$deployments))
-  # check media is a data.frame (if imported, i.e. if not NULL)
-  if (!is.null(package$data$media)) {
-    assertthat::assert_that(is.data.frame(package$data$media))
-  }
+  assertthat::assert_that(is.data.frame(package$data$media))
   # If no errors are encountered, TRUE is returned
   return(TRUE)
 }

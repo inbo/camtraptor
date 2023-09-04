ee4e36cb3716d04f6c7d01bb7f23f13690a6b1fc
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-09-04 11:18:17 +0200

    remove test for `datapkg` lifecycle warning

--- tests/testthat/test-get_scientific_name.R
+++ tests/testthat/test-get_scientific_name.R
@@ -18,14 +18,3 @@ test_that("Functions works case insensitively", {
   sc_names <- get_scientific_name(mica, c("beeCH MArten"))
   expect_equal(sc_names, c("Martes foina"))
 })
-
-test_that("Argument datapkg is deprecated: warning returned", {
-  expect_warning(
-    rlang::with_options(
-      lifecycle_verbosity = "warning",
-      get_scientific_name(datapkg = mica, vernacular_name = "beech marten")
-    ),
-    regexp = "The `datapkg` argument of `get_scientific_name()` is deprecated as of camtraptor 0.16.0.",
-    fixed = TRUE
-  )
-})

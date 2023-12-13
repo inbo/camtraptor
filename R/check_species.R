7c8615c03f586339512d12169b4b86ebdf0cc4e7
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-12-13 14:33:11 +0100

    Allow that check_species works with old format datapackages
    This commit is an improvement of e7c0796 for #292.

--- R/check_species.R
+++ R/check_species.R
@@ -47,7 +47,7 @@ check_species <- function(package = NULL,
 
   all_species <-
     get_species(package) %>%
-    dplyr::select(-"taxonID")
+    dplyr::select(-dplyr::any_of(c("taxonID", "taxonIDReference")))
   check_value(
     tolower(species),
     unlist(all_species) %>% tolower(),

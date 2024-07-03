f61c991e556acb0f39f1711272cded76a882f1aa
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-07-03 14:15:27 +0200

    Remove test about output of read_camtrapdp
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

--- tests/testthat/test-read_camtrapdp.R
+++ tests/testthat/test-read_camtrapdp.R
@@ -1,11 +1,3 @@
-test_that("read_camtrapdp() reads a Camtrap DP", {
-  skip_if_offline()
-  file <-
-    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
-  expect_identical(read_camtrapdp(file), camtrapdp::read_camtrapdp(file))
-})
-
-
 test_that("read_camtrap_dp is deprecated, but same result is returned", {
   skip_if_offline()
   file <-

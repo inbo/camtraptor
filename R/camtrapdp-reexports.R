506e8e6b57541e7b490c24b15816d8a79b5f236d
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-07-03 15:42:55 +0200

    Remove memoise from importFrom
    Co-Authored-By: Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>

--- R/camtrapdp-reexports.R
+++ R/camtrapdp-reexports.R
@@ -4,7 +4,6 @@
 camtrapdp::read_camtrapdp
 
 #' @importFrom camtrapdp example_dataset
-#' @importFrom memoise memoise
 #' @export
 #' @family sample data functions
 example_dataset <- memoise::memoise(camtrapdp::example_dataset)

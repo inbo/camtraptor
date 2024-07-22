48017604b7ce6546feead025d0b4997f02dc2f6a
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-07-22 17:32:56 +0200

    Fix #330

---
+++ R/utils-pipe.R
@@ -0,0 +1,16 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
+# Created with usethis::use_pipe(export = TRUE)
+# `@importFrom dplyr %>%` is defined at camtraptor-package, so
+# `@importFrom magrittr %>%` is not necessary here.

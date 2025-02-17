0b080d8287de15089f3bc97c49bb38b60b718bf3
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-02-17 23:00:27 +0100

    Add some extra testx

--- tests/testthat/test-get_detection_history.R
+++ tests/testthat/test-get_detection_history.R
@@ -1,4 +1,4 @@
-test_that("Camera operation matrix input, `cam_op`, is a matrix", {
+test_that("Camera operation matrix input, `cam_op`, has right format", {
   cam_op <- get_cam_op(mica)
   rec_table <- get_record_table(mica)
   species <- "Anas platyrhynchos"
@@ -31,7 +31,7 @@ test_that("Camera operation matrix input, `cam_op`, is a matrix", {
 }
 )
 
-test_that("Record table input, `rec_table`, is a tibble", {
+test_that("Record table input, `rec_table`, has right format", {
   cam_op <- get_cam_op(mica)
   rec_table <- get_record_table(mica)
   species <- "Anas platyrhynchos"
@@ -61,5 +61,102 @@ test_that("Record table input, `rec_table`, is a tibble", {
                "`rec_table` must be a tibble data.frame.",
                fixed = TRUE
   )
+  
+  rec_table_no_n <- rec_table
+  rec_table_no_n$n <- NULL
+  expect_error(
+    get_detection_history(cam_op = cam_op,
+                          rec_table = rec_table_no_n,
+                          species = species,
+                          output = output,
+                          occasion_length = occasion_length),
+    paste0("Invalid record table. Must contain at least the columns: ",
+           "`Station`, `Date`, `Species` and `n`."),
+    fixed = TRUE
+  )
 }
 )
+
+test_that("Check species", {
+  cam_op <- get_cam_op(mica)
+  rec_table <- get_record_table(mica)
+  output <- "binary"
+  occasion_length <- 1
+  expect_error(get_detection_history(cam_op = cam_op,
+                                     rec_table = rec_table,
+                                     species = NULL,
+                                     output = output,
+                                     occasion_length = occasion_length),
+               "`species` must be a character vector of lenght 1.",
+               fixed = TRUE
+  )
+  expect_error(get_detection_history(cam_op = cam_op,
+                                     rec_table = rec_table,
+                                     species = 1,
+                                     output = output,
+                                     occasion_length = occasion_length),
+               "`species` must be a character vector of lenght 1.",
+               fixed = TRUE
+  )
+  expect_error(
+    get_detection_history(cam_op = cam_op,
+                          rec_table = rec_table,
+                          species = c("Anas platyrhynchos", "Anas strepera"),
+                          output = output,
+                          occasion_length = occasion_length),
+    "`species` must be a character vector of lenght 1.",
+    fixed = TRUE
+  )
+  expect_error(
+    get_detection_history(cam_op = cam_op,
+                          rec_table = rec_table,
+                          species = "not a species",
+                          output = output,
+                          occasion_length = occasion_length),
+    paste0("Invalid value for species parameter: not a species.\n",
+           "Valid inputs are: Anas platyrhynchos, Anas strepera, Ardea, ",
+           "Ardea cinerea, Castor fiber, Homo sapiens, Martes foina, ",
+           "Mustela putorius and Vulpes vulpes"),
+    fixed = TRUE
+  )
+})
+
+test_that("Check output", {
+  cam_op <- get_cam_op(mica)
+  rec_table <- get_record_table(mica)
+  species <- "Anas platyrhynchos"
+  output <- 5
+  occasion_length <- 1
+  expect_error(get_detection_history(cam_op = cam_op,
+                                     rec_table = rec_table,
+                                     species = "Anas platyrhynchos",
+                                     output = output,
+                                     occasion_length = occasion_length),
+               "`output` must be a character vector of lenght 1.",
+               fixed = TRUE
+  )
+  output <- c(5,2)
+  expect_error(get_detection_history(cam_op = cam_op,
+                                     rec_table = rec_table,
+                                     species = "Anas platyrhynchos",
+                                     output = output,
+                                     occasion_length = occasion_length),
+               "`output` must be a character vector of lenght 1.",
+               fixed = TRUE
+  )
+  cam_op <- get_cam_op(mica)
+  rec_table <- get_record_table(mica)
+  species <- "Anas platyrhynchos"
+  output <- "wrong"
+  occasion_length <- 1
+  expect_error(
+    get_detection_history(cam_op = cam_op,
+                          rec_table = rec_table,
+                          species = "Anas platyrhynchos",
+                          output = output,
+                          occasion_length = occasion_length),
+    paste0("Invalid value for output parameter: wrong.\n",
+           "Valid inputs are: binary, n_observations and n_individuals"),
+    fixed = TRUE
+  )
+})

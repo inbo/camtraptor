fe01d4d2d2857a6638c8512e9a974e89985b907c
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-07-22 16:10:08 +0200

    Deprecate get_n_obs()
    Update documentation. Among others, remove from family "exploratory functions".

--- R/get_n_obs.R
+++ R/get_n_obs.R
@@ -1,88 +1,29 @@
 #' Get number of observations for each deployment
 #'
-#' Gets the number of observations per deployment.
-#' The number of observations is defined as the number of distinct sequences
-#' (`sequenceID`).
-#'
-#' @inheritParams n_species
-#' @return A tibble data frame with the following columns:
-#' - `deploymentID`: Deployment unique identifier.
-#' - `scientificName`: Scientific name of the species.
-#' - `n`: Number of observations.
-#' @family exploration functions
-#' @export
-#' @examples
-#' x <- example_dataset()
+#' `r lifecycle::badge("deprecated")`
 #' 
-#' # Get number of individuals for each species
-#' get_n_obs(x)
+#' Gets the number of observations per deployment.
+#' 
+#' It is deprecated as of camtraptor 1.0.0. Please use [summarize_observations()]
+#' instead.
 #'
-#' # Use `filter_observations()` to filter on  scientific name
-#' x %>%
-#'   filter_observations(
-#'     scientificName %in% c("Anas platyrhynchos", "Vulpes vulpes")
-#'   ) %>%
-#'   get_n_obs()
-#'
-#' # Use `filter_observations()` to filter on vernacular name
-#' x %>%
-#'   filter_observations(taxon.vernacularNames.eng == "mallard") %>%
-#'   get_n_obs()
-#'
-#' # Get number of individuals per deployment
-#' x %>%
-#'   get_n_obs() %>%
-#'   dplyr::group_by(.data$deploymentID) %>%
-#'   dplyr::summarise(n = sum(.data$n)) %>%
-#'   dplyr::ungroup()
+#' @inheritParams summarize_deployments
+#' @return A tibble data frame with following columns:
+#'   - `deploymentID`: deployment unique identifier.
+#'   - `n_observations`: intger vector with the number of observations.
+#'   - `sum_count`: integer vector with the sum of the count of individuals.
+#'   - `rai`: numeric vector with the relative abundance index (RAI) based on
+#'    number of observations.
+#'   - `rai_individuals`: numeric vector with the RAI based on the count of
+#'    individuals.
+#' @export
 get_n_obs <- function(x) {
-  # Check camera trap data package
-  camtrapdp::check_camtrapdp(x)
-  
-  # Extract observations and deployments
-  observations <- observations(x)
-  deployments <- deployments(x)
-
-  deploymentID <- purrr::pluck(deployments, "deploymentID")
-
-  deployments_no_obs <- get_dep_no_obs(x)
-
-  # Get number of observations collected by each deployment for each species
-  n_obs <-
-    observations %>%
-    dplyr::group_by(.data$deploymentID, .data$scientificName) %>%
-    dplyr::summarise(n = dplyr::n_distinct(.data$sequenceID)) %>%
-    dplyr::ungroup()
-
-  # Get all combinations deployments - scientific name
-  combinations_dep_species <-
-    expand.grid(
-      deploymentID,
-      unique(purrr::pluck(observations, "scientificName"))
-    ) %>%
-    dplyr::rename(deploymentID = "Var1", scientificName = "Var2") %>%
-    dplyr::as_tibble()
-
-  # Set 0 to combinations without observations (i.e. n = NA after join)
-  n_obs <-
-    combinations_dep_species %>%
-    dplyr::left_join(n_obs,
-      by = c("deploymentID", "scientificName")
-    ) %>%
-    dplyr::mutate(n = ifelse(is.na(.data$n), 0, .data$n)) %>%
-    dplyr::mutate(n = as.integer(.data$n))
-
-  if (is.null(species)) {
-    # Sum all observations per deployment
-    n_obs <-
-      n_obs %>%
-      dplyr::group_by(.data$deploymentID) %>%
-      dplyr::summarise(n = sum(.data$n)) %>%
-      dplyr::ungroup()
-  }
-
-  # Order result by deployments and follow same order as in deployments df
-  deployments %>%
-    dplyr::select("deploymentID") %>%
-    dplyr::left_join(n_obs, by = "deploymentID", multiple = "all")
+  lifecycle::deprecate_warn(
+    when = "1.0.0",
+    what = "get_effort()",
+    details = glue::glue(
+      "Please use `summarize_observations(x, group_by = \"deploymentID\")` ",
+      "instead."
+    )
+  )
 }

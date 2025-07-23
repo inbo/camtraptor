dc6270673d6c01b96fa92f4d80c5575715704808
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-07-23 12:42:06 +0200

    Deprecate get_n_indviduals

--- R/get_n_individuals.R
+++ R/get_n_individuals.R
@@ -1,85 +1,45 @@
 #' Get number of individuals for each deployment
 #'
+#' `r lifecycle::badge("deprecated")`
+#' 
 #' Gets the number of individuals per deployment.
-#' The number of observed individuals is stored in column `count` of
-#' `observations`.
+#' 
+#' It is deprecated as of camtraptor 1.0.0. Please use [summarize_observations()]
+#' instead.
 #'
-#' @inheritParams n_species
-#' @return A tibble data frame with the following columns:
-#' - `deploymentID`: Deployment unique identifier.
-#' - `scientificName`: Scientific name of the species. `NA` for not identified individuals.
-#' - `n`: Number of individuals.
-#' @family exploration functions
+#' @inheritParams get_n_obs
+#' @inherit get_n_obs return
 #' @export
 #' @examples
 #' x <- example_dataset()
 #' 
-#' # Get number of individuals for each species
+#' # Get sum of `individualCount` for each species
 #' get_n_individuals(x)
-#'
-#' # Use `filter_observations()` to filter on  scientific name
-#' x %>%
-#'   filter_observations(
-#'     scientificName %in% c("Anas platyrhynchos", "Vulpes vulpes")
-#'   ) %>%
-#'   get_n_individuals()
-#'
-#' # Use `filter_observations()` to filter on vernacular name
-#' x %>%
-#'   filter_observations(taxon.vernacularNames.eng == "mallard") %>%
-#'   get_n_individuals()
-#'
-#' # Get number of individuals per deployment
-#' x %>%
-#'   get_n_individuals() %>%
-#'   dplyr::group_by(.data$deploymentID) %>%
-#'   dplyr::summarise(n = sum(.data$n)) %>%
-#'   dplyr::ungroup()
-get_n_individuals <- function(x) {
-  # Check camera trap data package
-  camtrapdp::check_camtrapdp(x)
-  
-  # Extract observations and deployments
-  observations <- observations(x)
-  deployments <- deployments(x)
-
-  deploymentID <- purrr::pluck(deployments, "deploymentID")
-
-  deployments_no_obs <- get_dep_no_obs(x)
-
-  # Get number of individuals collected by each deployment for each species
-  n_individuals <-
-    observations %>%
-    dplyr::group_by(
-      .data$deploymentID,
-      .data$scientificName
-    ) %>%
-    dplyr::summarise(n = sum(.data$count)) %>%
-    dplyr::ungroup()
-
-  # Get all combinations deployments - scientific name
-  combinations_dep_species <-
-    expand.grid(
-      deploymentID,
-      unique(purrr::pluck(observations, "scientificName"))
-    ) %>%
-    dplyr::rename(
-      deploymentID = "Var1",
-      scientificName = "Var2"
-    ) %>%
-    dplyr::as_tibble()
-
-  # Set 0 to combinations without observed individuals (i.e. n = NA after join)
-  n_individuals <-
-    combinations_dep_species %>%
-    dplyr::left_join(n_individuals,
-      by = c("deploymentID", "scientificName")
-    ) %>%
-    dplyr::mutate(n = ifelse(is.na(.data$n), 0, .data$n)) %>%
-    dplyr::mutate(n = as.integer(.data$n))
-
-  # Order result by deployments and follow same order as in deployments df
-  deployments %>%
-    dplyr::select("deploymentID") %>%
-    dplyr::left_join(n_individuals, by = "deploymentID", multiple = "all")
+#' 
+#' # Get sum of `individualCount` of all species (unidentified included)
+#' get_n_individuals(x, species = NULL)
+#'  
+#' # Get number of observations of some species only, e.g. Anas platyrhynchos
+#' get_n_individuals(x, species = "Anas platyrhynchos")
+#' 
+#' # Specify sex
+#' get_n_individuals(x, sex = "female")
+#' 
+#' # Specify life stage
+#' get_n_individuals(x, life_stage = c("subadult", "adult"))
+get_n_individuals <- function(x,
+                              ...,
+                              species = "all",
+                              sex = NULL,
+                              life_stage = NULL) {
+  summarize_observations_for_deprecated_functions(
+    x,
+    ...,
+    species = species,
+    sex = sex,
+    life_stage = life_stage,
+    function_name = deparse(sys.call()[[1]])
+  ) %>%
+    dplyr::rename("n" = "sum_count") %>%
+    dplyr::select(dplyr::any_of(c("deploymentID", "scientificName", "n")))
 }

2697d035324d868dd167c522fdaa5ac1505f350c
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-07-23 12:41:51 +0200

    Improve get_n_obs() - refactor its deprecation

--- R/get_n_obs.R
+++ R/get_n_obs.R
@@ -2,28 +2,66 @@
 #'
 #' `r lifecycle::badge("deprecated")`
 #' 
-#' Gets the number of observations per deployment.
+#' Gets the number of (event-based) observations per deployment.
 #' 
 #' It is deprecated as of camtraptor 1.0.0. Please use [summarize_observations()]
 #' instead.
 #'
+#' @param species `r lifecycle::badge("deprecated")` Character with scientific names.
+#' Common names are not supported anymore as of camtraptor 1.0.0. Please, check
+#' `filter_observations()` to know how to filter by `scientificName`.
+#'   If `"all"` (default) all scientific names are automatically selected.
+#'   If `NULL` all observations of all species are taken into account.
+#' @param sex `r lifecycle::badge("deprecated")` Character defining the sex class to filter on, e.g. `"female"` or
+#'   `c("male", "unknown")`.
+#'   If `NULL` (default) all observations of all sex classes are taken into
+#'   account. Please, check
+#' `filter_observations()` to know how to filter by `sex`.
+#' @param life_stage `r lifecycle::badge("deprecated")` Character vector defining the life stage class to filter
+#'   on, e.g. `"adult"` or `c("subadult", "adult")`.
+#'   If `NULL` (default) all observations of all life stage classes are taken
+#'   into account. Please, check `filter_observations()` to know how to filter
+#'   by `lifeStage`.
+#' @param ... `r lifecycle::badge("deprecated")` filter predicates for filtering
+#'   on deployments are not supported anymore and will be ignored. Please use
+#'   `filter_deployments()` to filter on deployments.
 #' @inheritParams summarize_deployments
-#' @return A tibble data frame with following columns:
-#'   - `deploymentID`: deployment unique identifier.
-#'   - `n_observations`: intger vector with the number of observations.
-#'   - `sum_count`: integer vector with the sum of the count of individuals.
-#'   - `rai`: numeric vector with the relative abundance index (RAI) based on
-#'    number of observations.
-#'   - `rai_individuals`: numeric vector with the RAI based on the count of
-#'    individuals.
+#' @return A tibble data frame with the following columns:
+#' - `deploymentID`: Deployment unique identifier.
+#' - `scientificName`: Scientific name of the species.
+#'   This column is omitted if parameter `species = NULL`.
+#' - `n`: Number of observations.
 #' @export
-get_n_obs <- function(x) {
-  lifecycle::deprecate_warn(
-    when = "1.0.0",
-    what = "get_effort()",
-    details = glue::glue(
-      "Please use `summarize_observations(x, group_by = \"deploymentID\")` ",
-      "instead."
-    )
-  )
+#' @examples
+#' x <- example_dataset()
+#' 
+#' # Get number of observations for each species
+#' get_n_obs(x)
+#'
+#' # Get number of obs of all species, not identified individuals as well
+#' get_n_obs(x, species = NULL)
+#'  
+#' # Get number of observations of Anas platyrhynchos
+#' get_n_obs(x, species = "Anas platyrhynchos")
+#' 
+#' # Specify sex
+#' get_n_obs(x, sex = "female")
+#' 
+#' # Specify life stage
+#' get_n_obs(x, life_stage = c("subadult", "adult"))
+get_n_obs <- function(x,
+                      ...,
+                      species = "all",
+                      sex = NULL,
+                      life_stage = NULL) {
+  summarize_observations_for_deprecated_functions(
+    x,
+    ...,
+    species = species,
+    sex = sex,
+    life_stage = life_stage,
+    function_name = deparse(sys.call()[[1]])
+  ) %>%
+    dplyr::rename("n" = "n_observations") %>%
+    dplyr::select(dplyr::any_of(c("deploymentID", "scientificName", "n")))
 }
---
+++ R/utils-deprecated-get_functions.R
@@ -0,0 +1,127 @@
+#' Summarize observations for deprecated functions
+#'
+#' Summarizes observations of a Camera Trap Data Package object for the
+#' deprecated functions `get_n_obs()`, `get_n_individuals()`, `get_rai()` and
+#' `get_rai_individuals()`. The deprecation of these functions and their
+#' arguments `species`, `life_stage` and `sex` is also handled here.
+#'
+#' @param func_name Character, name of the deprecated function that is called.
+#'   One of`get_n_obs()`, `get_n_individuals()`, `get_rai()` and
+#'   `get_rai_individuals()`. Default: `NULL`.
+#' @inheritParams get_n_obs
+#' @return A tibble data frame with summarized observations as returned by
+#'   `summarize_observations()`.
+#' @noRd
+summarize_observations_for_deprecated_functions <- function(x,
+                                                            ...,
+                                                            species = "all",
+                                                            sex = NULL,
+                                                            life_stage = NULL,
+                                                            function_name = NULL) {
+  # Check function_name
+  assertthat::assert_that(
+    rlang::is_character(function_name, n = 1),
+    msg = "Argument `function_name` must be a single character string"
+  )
+  check_value(
+    function_name,
+    c("get_n_obs", "get_n_individuals", "get_rai", "get_rai_individuals"),
+    "function_name",
+    null_allowed = FALSE
+  )
+  
+  lifecycle::deprecate_warn(
+    when = "1.0.0",
+    what = paste0(function_name, "()"),
+    details = glue::glue(
+      "Please use `summarize_observations(x, group_by = c(\"deploymentID\", \"scientificName\")` ",
+      "instead."
+    )
+  )
+  
+  # Avoid to call variables like column names to make life easier using
+  # filter_observations()
+  sex_values <- sex
+  
+  # Check `species` values
+  check_value(
+    species,
+    c("all", dplyr::pull(taxa(x), "scientificName")),
+    "species",
+    null_allowed = TRUE
+  )
+  # Check `sex` values
+  check_value(
+    sex_values,
+    unique(dplyr::pull(observations(x), "sex")),
+    "sex",
+    null_allowed = TRUE
+  )
+  # Check `life_stage` values
+  check_value(
+    life_stage,
+    unique(dplyr::pull(observations(x), "lifeStage")),
+    "lifeStage",
+    null_allowed = TRUE
+  )
+  
+  # Return warning if ellipses are used
+  if (length(list(...)) > 0) {
+    lifecycle::deprecate_warn(
+      when = "1.0.0",
+      what = paste0(function_name, "(...)"),
+      details = glue::glue(
+        "Filtering deprecates passed via `...` are deprecated as of ",
+        "camtraptor 1.0.0 and are ignored. Please, use ",
+        "`filter_deployments()` to filter on deployments."
+      )
+    )
+  }
+  
+  # Filter by `sex` and deprecate it
+  if (!is.null(sex_values) && !"all" %in% sex_values) {
+    lifecycle::deprecate_warn(
+      when = "1.0.0",
+      what = paste0(function_name, "(sex)"),
+      details = glue::glue(
+        "Argument `sex` is deprecated as of camtraptor 1.0.0. Please, use ",
+        "`filter_observations()` to filter by `sex`."
+      )
+    )
+    x <- filter_observations(x, sex %in% sex_values)
+  }
+  
+  # Filter by `lifeStage`
+  if (!is.null(life_stage)) {
+    lifecycle::deprecate_warn(
+      when = "1.0.0",
+      what = paste0(function_name, "(life_stage)"),
+      details = glue::glue(
+        "Argument `life_stage` is deprecated as of camtraptor 1.0.0. Please, ",
+        "use `filter_observations()` to filter by `lifeStage`."
+      )
+    )
+    x <- filter_observations(x, lifeStage %in% life_stage)
+  }
+  
+  # Filter by `species`
+  if (is.null(species)) {
+    summary <- summarize_observations(x, group_by = "deploymentID")
+  } else {
+    if (!"all" %in% species) {
+      lifecycle::deprecate_warn(
+        when = "1.0.0",
+        what = paste0(function_name, "(species)"),
+        details = glue::glue(
+          "Argument `species` is deprecated as of camtraptor 1.0.0. Please, ",
+          "use `filter_observations()` to filter by `scientificName`."
+        )
+      )
+      x <- filter_observations(x, scientificName %in% species)
+    }
+    summary <- summarize_observations(
+      x,
+      group_by = c("deploymentID", "scientificName")
+    )
+  }
+}

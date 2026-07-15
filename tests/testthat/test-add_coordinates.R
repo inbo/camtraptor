d5c922fff1a975d2d55b47c318e41df191d2f282
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-07-15 11:40:33 +0200

    Add comment

--- tests/testthat/test-add_coordinates.R
+++ tests/testthat/test-add_coordinates.R
@@ -27,6 +27,7 @@ test_that("add_coordinates doesn't add coordinates if already present", {
     )
   )
   
+  # Check the warning message
   expect_warning(
     add_coordinates(x) %>% add_coordinates(),
     regexp = "Coordinates are not added because they already present in observations.",

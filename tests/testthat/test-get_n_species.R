bfa180c62a6baf7391317bd7d2f6b5d26aad5414
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-07-20 12:11:47 +0200

    Remove duplicated tests for get_n_species

--- tests/testthat/test-get_n_species.R
+++
@@ -1,6 +0,0 @@
-test_that("get_n_species() is deprecated", {
-  skip_if_offline()
-  x <- example_dataset()
-  lifecycle::expect_deprecated(get_n_species(x),
-                               regex = "was deprecated in camtraptor 1.0.0.")
-})

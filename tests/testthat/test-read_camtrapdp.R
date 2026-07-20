d4c903cba96dde5908f72bff4cf421d8c46b8ab1
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-07-20 10:50:19 +0200

    Update test to use lifecycle deprecation

--- tests/testthat/test-read_camtrapdp.R
+++ tests/testthat/test-read_camtrapdp.R
@@ -2,13 +2,9 @@ test_that("read_camtrap_dp is deprecated, but same result is returned", {
   skip_if_offline()
   file <-
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
-  expect_warning(
+  lifecycle::expect_deprecated(
     read_camtrap_dp(file),
-    glue::glue("'read_camtrap_dp' is deprecated.",
-               "Use 'read_camtrapdp' instead.",
-               "See help(\"Deprecated\") and help(\"camtraptor-deprecated\").",
-               .sep = "\n"),
-    fixed = TRUE
+    regex = "was deprecated in camtraptor 1.0.0."
   )
   expect_identical(
     suppressWarnings(read_camtrap_dp(file)),
